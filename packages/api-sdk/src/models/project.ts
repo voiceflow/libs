2357c8d4dbbde6f9aeb23096f2a0ff66a994c2e8
Tyler Han <tylerhan97@gmail.com>
2020-09-14 17:47:45 -0400

    fix: project privacy (#22)
    * fix: project privacy
    
    * chore: remove unused value

--- packages/api-sdk/src/models/project.ts
+++ packages/api-sdk/src/models/project.ts
@@ -3,6 +3,11 @@ import * as s from 'superstruct';
 import { Member, SMember } from './member';
 import { BasePlatformData, SBasePlatformData, SCreatorID, SName, SPlatform, SProjectID, STeamID, STimestamp, SVersionID } from './shared';
 
+export enum ProjectPrivacy {
+  PUBLIC = 'public',
+  PRIVATE = 'private',
+}
+
 export const SProject = s.object({
   _id: SProjectID,
   teamID: STeamID,
@@ -15,6 +20,7 @@ export const SProject = s.object({
   platform: SPlatform,
   devVersion: s.optional(SVersionID),
   platformData: SBasePlatformData,
+  privacy: s.optional(s.enums([ProjectPrivacy.PRIVATE, ProjectPrivacy.PUBLIC])),
 });
 
 export type Project<P extends BasePlatformData, M extends BasePlatformData> = Omit<s.StructType<typeof SProject>, 'platformData'> & {
