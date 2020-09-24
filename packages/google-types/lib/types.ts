469420bbd57b03c1dd0bf999861e91367c56a256
Evgeny Zaytsev <overhawlin@gmail.com>
2020-09-24 16:28:04 +0300

    fix: add invocation name (#3)
    * fix: add invocation name
    
    * fix: add locales

--- packages/google-types/lib/types.ts
+++ packages/google-types/lib/types.ts
@@ -24,6 +24,42 @@ export enum ExpressionType {
   ADVANCE = 'advance',
 }
 
+export enum Locale {
+  EN_AU = 'en-AU',
+  EN_CA = 'en-CA',
+  EN_GB = 'en-GB',
+  EN_IN = 'en-IN',
+  EN_BE = 'en-BE',
+  EN_SG = 'en-SG',
+  EN_US = 'en-US',
+  ZH_HK = 'zh-HK',
+  ZH_TW = 'zh-TW',
+  DA_DK = 'da-DK',
+  NL_NL = 'nl-NL',
+  NL_BE = 'nl-BE',
+  FE_FR = 'fr-FR',
+  FE_CA = 'fr-CA',
+  FE_BE = 'fr-BE',
+  DE_DE = 'de-DE',
+  DE_AT = 'de-AT',
+  DE_CH = 'de-CH',
+  DE_BE = 'de-BE',
+  HI_IN = 'hi-IN',
+  ID_ID = 'id-ID',
+  IT_IT = 'it-IT',
+  JA_JP = 'ja-JP',
+  KO_KR = 'ko-KR',
+  NO_NO = 'no-NO',
+  PL_PL = 'pl-PL',
+  PT_BR = 'pt-BR',
+  RU_RU = 'ru-RU',
+  ES_ES = 'es-ES',
+  ES_419 = 'es-419',
+  SV_SE = 'sv-SE',
+  TH_TH = 'th-TH',
+  TR_TR = 'tr-TR',
+}
+
 export enum Voice {
   DEFAULT = 'default', // not a real voice (default no voice)
 }
--- packages/google-types/lib/version/publishing.ts
+++ packages/google-types/lib/version/publishing.ts
@@ -1,9 +1,13 @@
+import { Locale } from '@/types';
+
 export type GooglePublishing = {
   voice: string;
+  locales: Locale[];
   displayName: string;
   developerName: string;
   pronunciation: string;
   developerEmail: string;
+  invocationName: string;
   smallLogoImage: string;
   fullDescription: string;
   privacyPolicyUrl: string;
@@ -15,11 +19,13 @@ export type GooglePublishing = {
 
 export const defaultGooglePublishing = ({
   voice = '',
+  locales = [],
   displayName = '',
   developerName = '',
   pronunciation = '',
   smallLogoImage = '',
   developerEmail = '',
+  invocationName = '',
   fullDescription = '',
   largeBannerImage = '',
   privacyPolicyUrl = '',
@@ -28,9 +34,11 @@ export const defaultGooglePublishing = ({
   sampleInvocations = [],
 }: Partial<GooglePublishing> = {}): GooglePublishing => ({
   voice,
+  locales,
   displayName,
   developerName,
   pronunciation,
+  invocationName,
   smallLogoImage,
   developerEmail,
   fullDescription,
