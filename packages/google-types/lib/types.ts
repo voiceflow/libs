2cafa26c8947e9653be1454308423d5d7ffacf0e
Spacevash <nidhi.gaday@gmail.com>
2020-11-19 16:05:31 -0500

    fix: correct-language (CORE-4443) (#17)

--- packages/google-types/lib/types.ts
+++ packages/google-types/lib/types.ts
@@ -4,7 +4,6 @@ export type GoogleProfile = oauth2_v2.Schema$Userinfo;
 
 export enum Language {
   HK= 'zh-HK',
-  CN= 'zh-CN',
   TW= 'zh-TW',
   DA= 'da',
   NL= 'nl',
@@ -19,16 +18,25 @@ export enum Language {
   NO= 'no',
   PL= 'pl',
   PT= 'pt',
-  BR= 'pt-BR',
   RU= 'ru',
   ES= 'es',
   SV= 'sv',
   TH= 'th',
   TR= 'tr',
-  UK= 'uk',
 }
 
 export enum Locale {
+  // Chinese
+  ZH_HK = 'zh-HK',
+  ZH_TW = 'zh-TW',
+  // Danish
+  DA_DK = 'da-DK',
+
+  //Dutch
+  NL_NL = 'nl-NL',
+  NL_BE = 'nl-BE',
+
+  // English
   EN_AU = 'en-AU',
   EN_CA = 'en-CA',
   EN_GB = 'en-GB',
@@ -36,32 +44,56 @@ export enum Locale {
   EN_BE = 'en-BE',
   EN_SG = 'en-SG',
   EN_US = 'en-US',
-  ZH_HK = 'zh-HK',
-  ZH_TW = 'zh-TW',
-  ZH_CN = 'zh-CN',
-  DA_DK = 'da-DK',
-  NL_NL = 'nl-NL',
-  NL_BE = 'nl-BE',
+
+  // French
   FE_FR = 'fr-FR',
   FE_CA = 'fr-CA',
   FE_BE = 'fr-BE',
+
+  // German
   DE_DE = 'de-DE',
   DE_AT = 'de-AT',
   DE_CH = 'de-CH',
   DE_BE = 'de-BE',
+
+  // Hindi
   HI_IN = 'hi-IN',
+
+  // Indonasian
   ID_ID = 'id-ID',
+
+  // Italian
   IT_IT = 'it-IT',
+
+  // Japanese
   JA_JP = 'ja-JP',
+
+  // Korean
   KO_KR = 'ko-KR',
+
+  // Norwegian
   NO_NO = 'no-NO',
+
+  // Polish
   PL_PL = 'pl-PL',
+
+  // Portugese
   PT_BR = 'pt-BR',
+
+  // Russian
   RU_RU = 'ru-RU',
+
+  // Spanish
   ES_ES = 'es-ES',
   ES_419 = 'es-419',
+
+  // Swedish
   SV_SE = 'sv-SE',
+
+  // Thai
   TH_TH = 'th-TH',
+
+  // Turkish
   TR_TR = 'tr-TR',
 }
 
