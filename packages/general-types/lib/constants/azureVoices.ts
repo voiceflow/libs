8c2d67994a4b2d63350be3416a641eedd87ff286
Josh <31071869+JoshTheDeveloper@users.noreply.github.com>
2021-01-20 13:49:13 -0500

    feat: new azure voice const (#28)

---
+++ packages/general-types/lib/constants/azureVoices.ts
@@ -0,0 +1,188 @@
+// eslint-disable-next-line import/prefer-default-export
+export const AZURE_LOCALE_VOICE_META = {
+  'ar-EG': { language: 'Arabic (Arabic )', locale: 'ar-EG', voices: [{ gender: 'Female', voiceID: 'azure-ar-EG-Hoda' }] },
+  'ar-SA': { language: 'Arabic (Saudi Arabia)', locale: 'ar-SA', voices: [{ gender: 'Male', voiceID: 'azure-ar-SA-Naayf' }] },
+  'bg-BG': { language: 'Bulgarian (Bulgaria)', locale: 'bg-BG', voices: [{ gender: 'Male', voiceID: 'azure-bg-BG-Ivan' }] },
+  'ca-ES': { language: 'Catalan (Spain)', locale: 'ca-ES', voices: [{ gender: 'Female', voiceID: 'azure-ca-ES-HerenaRUS' }] },
+  'zh-HK': {
+    language: 'Chinese (Cantonese, Traditional)',
+    locale: 'zh-HK',
+    voices: [
+      { gender: 'Male', voiceID: 'azure-zh-HK-Danny' },
+      { gender: 'Female', voiceID: 'azure-zh-HK-TracyRUS' },
+    ],
+  },
+  'zh-CN': {
+    language: 'Chinese (Mandarin, Simplified)',
+    locale: 'zh-CN',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-zh-CN-HuihuiRUS' },
+      { gender: 'Male', voiceID: 'azure-zh-CN-Kangkang' },
+      { gender: 'Female', voiceID: 'azure-zh-CN-Yaoyao' },
+    ],
+  },
+  'zh-TW': {
+    language: 'Chinese (Taiwanese Mandarin)',
+    locale: 'zh-TW',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-zh-TW-HanHanRUS' },
+      { gender: 'Female', voiceID: 'azure-zh-TW-Yating' },
+      { gender: 'Male', voiceID: 'azure-zh-TW-Zhiwei' },
+    ],
+  },
+  'hr-HR': { language: 'Croatian (Croatia)', locale: 'hr-HR', voices: [{ gender: 'Male', voiceID: 'azure-hr-HR-Matej' }] },
+  'cs-CZ': { language: 'Czech (Czech Republic)', locale: 'cs-CZ', voices: [{ gender: 'Male', voiceID: 'azure-cs-CZ-Jakub' }] },
+  'da-DK': { language: 'Danish (Denmark)', locale: 'da-DK', voices: [{ gender: 'Female', voiceID: 'azure-da-DK-HelleRUS' }] },
+  'nl-NL': { language: 'Dutch (Netherlands)', locale: 'nl-NL', voices: [{ gender: 'Female', voiceID: 'azure-nl-NL-HannaRUS' }] },
+  'en-AU': {
+    language: 'English (Australia)',
+    locale: 'en-AU',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-en-AU-Catherine' },
+      { gender: 'Female', voiceID: 'azure-en-AU-HayleyRUS' },
+    ],
+  },
+  'en-CA': {
+    language: 'English (Canada)',
+    locale: 'en-CA',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-en-CA-HeatherRUS' },
+      { gender: 'Female', voiceID: 'azure-en-CA-Linda' },
+    ],
+  },
+  'en-IN': {
+    language: 'English (India)',
+    locale: 'en-IN',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-en-IN-Heera' },
+      { gender: 'Female', voiceID: 'azure-en-IN-PriyaRUS' },
+      { gender: 'Male', voiceID: 'azure-en-IN-Ravi' },
+    ],
+  },
+  'en-IE': { language: 'English (Ireland)', locale: 'en-IE', voices: [{ gender: 'Male', voiceID: 'azure-en-IE-Sean' }] },
+  'en-GB': {
+    language: 'English (United Kingdom)',
+    locale: 'en-GB',
+    voices: [
+      { gender: 'Male', voiceID: 'azure-en-GB-George' },
+      { gender: 'Female', voiceID: 'azure-en-GB-HazelRUS' },
+      { gender: 'Female', voiceID: 'azure-en-GB-Susan' },
+    ],
+  },
+  'en-US': {
+    language: 'English (United States)',
+    locale: 'en-US',
+    voices: [
+      { gender: 'Male', voiceID: 'azure-en-US-BenjaminRUS' },
+      { gender: 'Male', voiceID: 'azure-en-US-GuyRUS' },
+      { gender: 'Female', voiceID: 'azure-en-US-AriaRUS' },
+      { gender: 'Female', voiceID: 'azure-en-US-ZiraRUS' },
+    ],
+  },
+  'fi-FI': { language: 'Finnish (Finland)', locale: 'fi-FI', voices: [{ gender: 'Female', voiceID: 'azure-fi-FI-HeidiRUS' }] },
+  'fr-CA': {
+    language: 'French (Canada)',
+    locale: 'fr-CA',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-fr-CA-Caroline' },
+      { gender: 'Female', voiceID: 'azure-fr-CA-HarmonieRUS' },
+    ],
+  },
+  'fr-FR': {
+    language: 'French (France)',
+    locale: 'fr-FR',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-fr-FR-HortenseRUS' },
+      { gender: 'Female', voiceID: 'azure-fr-FR-Julie' },
+      { gender: 'Male', voiceID: 'azure-fr-FR-Paul' },
+    ],
+  },
+  'fr-CH': { language: 'French (Switzerland)', locale: 'fr-CH', voices: [{ gender: 'Male', voiceID: 'azure-fr-CH-Guillaume' }] },
+  'de-AT': { language: 'German (Austria)', locale: 'de-AT', voices: [{ gender: 'Male', voiceID: 'azure-de-AT-Michael' }] },
+  'de-DE': {
+    language: 'German (Germany)',
+    locale: 'de-DE',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-de-DE-HeddaRUS' },
+      { gender: 'Male', voiceID: 'azure-de-DE-Stefan' },
+    ],
+  },
+  'de-CH': { language: 'German (Switzerland)', locale: 'de-CH', voices: [{ gender: 'Male', voiceID: 'azure-de-CH-Karsten' }] },
+  'el-GR': { language: 'Greek (Greece)', locale: 'el-GR', voices: [{ gender: 'Male', voiceID: 'azure-el-GR-Stefanos' }] },
+  'he-IL': { language: 'Hebrew (Israel)', locale: 'he-IL', voices: [{ gender: 'Male', voiceID: 'azure-he-IL-Asaf' }] },
+  'hi-IN': {
+    language: 'Hindi (India)',
+    locale: 'hi-IN',
+    voices: [
+      { gender: 'Male', voiceID: 'azure-hi-IN-Hemant' },
+      { gender: 'Female', voiceID: 'azure-hi-IN-Kalpana' },
+    ],
+  },
+  'hu-HU': { language: 'Hungarian (Hungary)', locale: 'hu-HU', voices: [{ gender: 'Male', voiceID: 'azure-hu-HU-Szabolcs' }] },
+  'id-ID': { language: 'Indonesian (Indonesia)', locale: 'id-ID', voices: [{ gender: 'Male', voiceID: 'azure-id-ID-Andika' }] },
+  'it-IT': {
+    language: 'Italian (Italy)',
+    locale: 'it-IT',
+    voices: [
+      { gender: 'Male', voiceID: 'azure-it-IT-Cosimo' },
+      { gender: 'Female', voiceID: 'azure-it-IT-LuciaRUS' },
+    ],
+  },
+  'ja-JP': {
+    language: 'Japanese (Japan)',
+    locale: 'ja-JP',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-ja-JP-Ayumi' },
+      { gender: 'Female', voiceID: 'azure-ja-JP-HarukaRUS' },
+      { gender: 'Male', voiceID: 'azure-ja-JP-Ichiro' },
+    ],
+  },
+  'ko-KR': { language: 'Korean (Korea)', locale: 'ko-KR', voices: [{ gender: 'Female', voiceID: 'azure-ko-KR-HeamiRUS' }] },
+  'ms-MY': { language: 'Malay (Malaysia)', locale: 'ms-MY', voices: [{ gender: 'Male', voiceID: 'azure-ms-MY-Rizwan' }] },
+  'nb-NO': { language: 'Norwegian (Bokmål, Norway)', locale: 'nb-NO', voices: [{ gender: 'Female', voiceID: 'azure-nb-NO-HuldaRUS' }] },
+  'pl-PL': { language: 'Polish (Poland)', locale: 'pl-PL', voices: [{ gender: 'Female', voiceID: 'azure-pl-PL-PaulinaRUS' }] },
+  'pt-BR': {
+    language: 'Portuguese (Brazil)',
+    locale: 'pt-BR',
+    voices: [
+      { gender: 'Male', voiceID: 'azure-pt-BR-Daniel' },
+      { gender: 'Female', voiceID: 'azure-pt-BR-HeloisaRUS' },
+    ],
+  },
+  'pt-PT': { language: 'Portuguese (Portugal)', locale: 'pt-PT', voices: [{ gender: 'Female', voiceID: 'azure-pt-PT-HeliaRUS' }] },
+  'ro-RO': { language: 'Romanian (Romania)', locale: 'ro-RO', voices: [{ gender: 'Male', voiceID: 'azure-ro-RO-Andrei' }] },
+  'ru-RU': {
+    language: 'Russian (Russia)',
+    locale: 'ru-RU',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-ru-RU-EkaterinaRUS' },
+      { gender: 'Female', voiceID: 'azure-ru-RU-Irina' },
+      { gender: 'Male', voiceID: 'azure-ru-RU-Pavel' },
+    ],
+  },
+  'sk-SK': { language: 'Slovak (Slovakia)', locale: 'sk-SK', voices: [{ gender: 'Male', voiceID: 'azure-sk-SK-Filip' }] },
+  'sl-SI': { language: 'Slovenian (Slovenia)', locale: 'sl-SI', voices: [{ gender: 'Male', voiceID: 'azure-sl-SI-Lado' }] },
+  'es-MX': {
+    language: 'Spanish (Mexico)',
+    locale: 'es-MX',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-es-MX-HildaRUS' },
+      { gender: 'Male', voiceID: 'azure-es-MX-Raul' },
+    ],
+  },
+  'es-ES': {
+    language: 'Spanish (Spain)',
+    locale: 'es-ES',
+    voices: [
+      { gender: 'Female', voiceID: 'azure-es-ES-HelenaRUS' },
+      { gender: 'Female', voiceID: 'azure-es-ES-Laura' },
+      { gender: 'Male', voiceID: 'azure-es-ES-Pablo' },
+    ],
+  },
+  'sv-SE': { language: 'Swedish (Sweden)', locale: 'sv-SE', voices: [{ gender: 'Female', voiceID: 'azure-sv-SE-HedvigRUS' }] },
+  'ta-IN': { language: 'Tamil (India)', locale: 'ta-IN', voices: [{ gender: 'Male', voiceID: 'azure-ta-IN-Valluvar' }] },
+  'te-IN': { language: 'Telugu (India)', locale: 'te-IN', voices: [{ gender: 'Female', voiceID: 'azure-te-IN-Chitra' }] },
+  'th-TH': { language: 'Thai (Thailand)', locale: 'th-TH', voices: [{ gender: 'Male', voiceID: 'azure-th-TH-Pattara' }] },
+  'tr-TR': { language: 'Turkish (Turkey)', locale: 'tr-TR', voices: [{ gender: 'Female', voiceID: 'azure-tr-TR-SedaRUS' }] },
+  'vi-VN': { language: 'Vietnamese (Vietnam)', locale: 'vi-VN', voices: [{ gender: 'Male', voiceID: 'azure-vi-VN-An' }] },
+};
--- packages/general-types/lib/constants/index.ts
+++ packages/general-types/lib/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './regex';
 export * from './intent';
 export * from './slot';
+export * from './azureVoices';
