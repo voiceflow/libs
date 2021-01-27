c26a003b5adff658165aa7d7cf8767d6ad62e362
Zhi Heng (Brennan) Lu <32404412+zhihil@users.noreply.github.com>
2021-01-27 17:36:56 -0500

    feat: exposing the other sub-types of GeneralTrace (#36)
    Co-authored-by: Zhiheng Lu <brennan@voiceflow.com>

--- packages/general-types/lib/trace/index.ts
+++ packages/general-types/lib/trace/index.ts
@@ -5,6 +5,11 @@ import { TraceFrame as SpeakTrace } from '@/nodes/speak';
 import { TraceFrame as StreamTrace } from '@/nodes/stream';
 import { TraceFrame, TraceType } from '@/nodes/types';
 
+export { TraceFrame as ExitTrace } from '@/nodes/exit';
+export { TraceFrame as FlowTrace } from '@/nodes/flow';
+export { TraceFrame as ChoiceTrace } from '@/nodes/interaction';
+export { TraceFrame as SpeakTrace } from '@/nodes/speak';
+export { TraceFrame as StreamTrace } from '@/nodes/stream';
 export type DebugTrace = TraceFrame<TraceType.DEBUG, { message: string }>;
 export type BlockTrace = TraceFrame<TraceType.BLOCK, { blockID: string }>;
 
