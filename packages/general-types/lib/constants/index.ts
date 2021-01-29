f350d4108d22951522bd511c30f098f906aca99a
Evgeny Zaytsev <overhawlin@gmail.com>
2021-01-29 12:10:24 +0300

    feat: move regex to common repo (#37)

--- packages/general-types/lib/constants/index.ts
+++ packages/general-types/lib/constants/index.ts
@@ -1,5 +1,4 @@
 export * from './slot';
-export * from './regex';
 export * from './intent';
 export * from './device';
 export * from './azureVoices';
--- packages/general-types/lib/constants/regex.ts
+++
@@ -1,5 +0,0 @@
-export const SLOT_REGEXP = /{{\[([^ .[\]{}]*?)]\.([^ .[\]{}]*?)}}/g;
-
-export const IS_VARIABLE_REGEXP = /^{.*}$/;
-
-export const READABLE_VARIABLE_REGEXP = /\{([a-zA-Z0-9_]{1,32})\}/g;
