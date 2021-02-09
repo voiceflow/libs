d1f3060d69ec50332e6032fbf4406e50c2fc4b83
Tyler Han <tyler@voiceflow.com>
2021-02-09 12:00:33 -0500

    fix: add name and emails

--- packages/general-types/lib/constants/slot.ts
+++ packages/general-types/lib/constants/slot.ts
@@ -9,6 +9,7 @@ export enum SlotType {
   // COLOR = 'VF.COLOR',
   // COUNTRY = 'VF.COUNTRY',
   NAME = 'VF.NAME',
+  EMAIL = 'VF.EMAIL',
 }
 
 export type SlotTypeValue = { name: SlotType; label: string; values: Array<string>; regex?: string };
@@ -17,12 +18,21 @@ type ObjectKeys = {
   [key: string]: Array<SlotTypeValue>;
 };
 
+const NUMBERS = ['-43', '0', '35.5', '8', '520', '23599', '325', '15', '84730909029', '6947'];
+
+const EMAIL: SlotTypeValue = {
+  name: SlotType.EMAIL,
+  label: 'Email',
+  values: ['user@voiceflow.com', 'professor@utoronto.edu', 'person_name@gmail.com', 'username123.signature@hotmail.com', 'researcher@charity.org'],
+  regex: '^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$',
+};
+
 export const SlotTypes: ObjectKeys = {
   [Language.EN]: [
     {
       name: SlotType.NUMBER,
       label: 'Number',
-      values: ['-43', '0', '35.5', '8', '520', '23599', '325', '15', '84730909029', '6947'],
+      values: NUMBERS,
     },
     {
       name: SlotType.NAME,
@@ -50,6 +60,7 @@ export const SlotTypes: ObjectKeys = {
         'sam',
       ],
     },
+    EMAIL,
     // {
     //   name: SlotType.COLOR,
     //   label: 'Color',
@@ -60,46 +71,118 @@ export const SlotTypes: ObjectKeys = {
     {
       name: SlotType.NUMBER,
       label: 'Nummer',
-      values: ['-43', '0', '35.5', '8', '520', '23599', '325', '15', '84730909029', '6947'],
+      values: NUMBERS,
     },
+    {
+      name: SlotType.NAME,
+      label: 'Name',
+      values: [
+        'Schmidt',
+        'Müller',
+        'Daniel',
+        'Michael',
+        'Lucas',
+        'Michelle',
+        'Laura',
+        'Lisa',
+        'Christina',
+        'Sabrina',
+        'Julia',
+        'Alexander',
+        'Hans',
+        'Nicole',
+      ],
+    },
+    EMAIL,
   ],
   [Language.FR]: [
     {
       name: SlotType.NUMBER,
       label: 'Nombre',
-      values: ['-43', '0', '35.5', '8', '520', '23599', '325', '15', '84730909029', '6947'],
+      values: NUMBERS,
     },
+    {
+      name: SlotType.NAME,
+      label: 'Name',
+      values: ['Martin', 'Thomas', 'Jacques', 'Jean', 'Pierre', 'Marie', 'Nicolas', 'Emma', 'Louise', 'Alice', 'Hugo', 'Liam', 'Mohamed', 'Lea'],
+    },
+    EMAIL,
+  ],
+  [Language.PT]: [
+    {
+      name: SlotType.NUMBER,
+      label: 'Número',
+      values: NUMBERS,
+    },
+    {
+      name: SlotType.NAME,
+      label: 'Nombre',
+      values: [
+        'alejandro',
+        'xavier',
+        'savannah',
+        'santiago',
+        'sofia',
+        'camila',
+        'mateo',
+        'nicolas',
+        'samuel',
+        'valeria',
+        'lucas',
+        'diego',
+        'juan',
+        'isabella',
+      ],
+    },
+    EMAIL,
   ],
   [Language.ES]: [
     {
       name: SlotType.NUMBER,
       label: 'Número',
-      values: ['-43', '0', '35.5', '8', '520', '23599', '325', '15', '84730909029', '6947'],
+      values: NUMBERS,
     },
-    // {
-    //   name: SlotType.NAME,
-    //   label: 'Nombre',
-    //   values: [
-    //     'alejandro',
-    //     'xavier',
-    //     'savannah',
-    //     'santiago',
-    //     'sofia',
-    //     'camila',
-    //     'mateo',
-    //     'nicolas',
-    //     'samuel',
-    //     'valeria',
-    //     'lucas',
-    //     'diego',
-    //     'juan',
-    //     'isabella',
-    //   ],
-    // },
+    {
+      name: SlotType.NAME,
+      label: 'Nombre',
+      values: [
+        'alejandro',
+        'xavier',
+        'savannah',
+        'santiago',
+        'sofia',
+        'camila',
+        'mateo',
+        'nicolas',
+        'samuel',
+        'valeria',
+        'lucas',
+        'diego',
+        'juan',
+        'isabella',
+      ],
+    },
+    EMAIL,
     // {
     //   name: SlotType.COLOR,
     //   label: 'Color',
     //   values: ['negro', 'blanco', 'rojo', 'azul', 'verde', 'marrón', 'verde', 'naranja', 'amarillo', 'cian', 'violeta'],
     // },
+  ],
+  [Language.ZH]: [
+    {
+      name: SlotType.NUMBER,
+      label: '数字',
+      values: NUMBERS,
+    },
+    EMAIL,
+  ],
+  [Language.JA]: [
+    {
+      name: SlotType.NUMBER,
+      label: '数',
+      values: NUMBERS,
+    },
+    EMAIL,
   ],
 };
