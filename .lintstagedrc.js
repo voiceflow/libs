27d28c7e7d3bfa8ac779ec5371c9de4e7dbc4158
xavidop <xavi_tb@hotmail.com>
2021-05-12 13:35:01 +0200

    fix: move eslint to pre-commit husky hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   'packages/**/package.json': ['fixpack'],
-  'packages/**/lib/**/**.{js,ts}': ['eslint --fix', 'prettier-eslint --write'],
-  'packages/**/src/**/**.{js,ts}': ['eslint --fix', 'prettier-eslint --write'],
+  'packages/**/lib/**/**.{js,ts}': ['prettier --write'],
+  'packages/**/src/**/**.{js,ts}': ['prettier --write'],
 };
