6d42e24e5b9ca6f24d302d0baaeab812f227fb78
Ben Teichman <ben@voiceflow.com>
2021-05-12 15:21:35 -0400

    refactor: cleanup eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const packagesDir = path.join(__dirname, 'packages');
+const packages = fs.readdirSync(packagesDir);
+
 module.exports = {
-  extends: ['@voiceflow/eslint-config', '@voiceflow/eslint-config/typescript', '@voiceflow/eslint-config/utility'],
+  extends: ['@voiceflow/eslint-config'],
+  rules: {
+    'import/prefer-default-export': 'off',
+  },
   overrides: [
+    {
+      files: ['./*.js'],
+      extends: ['@voiceflow/eslint-config/utility'],
+    },
     {
       files: ['*.ts'],
       rules: {
-        'no-underscore-dangle': 'off',
         'no-shadow': 'off',
-      }
+      },
     },
-    {
-      files: ['packages/**/tests/**/*'],
-      extends: ['@voiceflow/eslint-config/mocha'],
-    },
+    ...packages.map((pkg) => ({
+      files: [`packages/${pkg}/**/*`],
+      extends: ['@voiceflow/eslint-config/typescript'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: path.join(packagesDir, pkg, 'tsconfig.json'),
+          },
+        },
+      },
+    })),
   ],
 };
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   'packages/**/package.json': ['fixpack'],
-  'packages/**/lib/**/**.{js,ts}': ['eslint --fix', 'prettier --write'],
-  'packages/**/src/**/**.{js,ts}': ['eslint --fix', 'prettier --write'],
+  'packages/**/lib/**/**.{js,ts}': ['eslint --fix'],
+  'packages/**/src/**/**.{js,ts}': ['eslint --fix'],
 };
