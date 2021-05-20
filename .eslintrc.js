2111a098a0d5976762acd42b9f5ab972a6229608
Ben Teichman <ben@voiceflow.com>
2021-05-20 11:50:51 -0400

    build(lint): use monorepo lint utility (VF-000)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-const packagesDir = path.join(__dirname, 'packages');
-const packages = fs.readdirSync(packagesDir);
+const monorepoOverrides = require('@voiceflow/eslint-config/overrides/monorepo');
 
 module.exports = {
   extends: ['@voiceflow/eslint-config'],
@@ -14,22 +10,6 @@ module.exports = {
       files: ['*.js'],
       extends: ['@voiceflow/eslint-config/utility'],
     },
-    {
-      files: ['*.ts'],
-      rules: {
-        'no-shadow': 'off',
-      },
-    },
-    ...packages.map((pkg) => ({
-      files: [`packages/${pkg}/**/*`],
-      extends: ['@voiceflow/eslint-config/typescript'],
-      settings: {
-        'import/resolver': {
-          typescript: {
-            project: path.join(packagesDir, pkg, 'tsconfig.json'),
-          },
-        },
-      },
-    })),
+    ...monorepoOverrides(__dirname),
   ],
 };
