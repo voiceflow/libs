0d89f6cf89696566f8982b8a7460d3640b78aebd
leartgjoni-voiceflow <53058131+leartgjoni-voiceflow@users.noreply.github.com>
2021-07-09 16:58:30 +0200

    feat: image step and node (VF-1232) (#27)
    * feat: image step and node
    
    * chore: node type
    
    * chore: reuse general visual node

--- packages/google-types/src/nodes/index.ts
+++ packages/google-types/src/nodes/index.ts
@@ -1,3 +1,5 @@
+import * as DFESImage from '@voiceflow/general-types/build/nodes/visual';
+
 import * as Capture from './capture';
 import * as Card from './card';
 import * as Command from './command';
@@ -7,8 +9,13 @@ import * as Stream from './stream';
 
 export * from './types';
 
+// gactions
 export type GoogleSteps = Card.Step | Stream.Step | Command.Step | Interaction.Step | Intent.Step | Capture.Step;
 
 export type GoogleNodes = Card.Node | Stream.Node | Interaction.Node | Capture.Node;
 
 export type GoogleCommand = Command.Command | Intent.Command;
+
+// dialogflow es
+export type DialogflowESSteps = Card.Step | Command.Step | Interaction.Step | Intent.Step | Capture.Step | DFESImage.Step;
+export type DialogflowESNodes = Card.Node | Interaction.Node | Capture.Node | DFESImage.Node;
