4f99e08c48818699c6b131171e8d838d48c6dfd7
Evgeny Zaytsev <overhawlin@gmail.com>
2021-08-06 21:36:21 +0300

    feat: text step (#37)

---
+++ packages/general-types/src/nodes/text.ts
@@ -0,0 +1,77 @@
+import { BaseElement, BaseText } from 'slate';
+
+import { CanvasNodeVisibility } from '@/types';
+
+import { BaseNode, BaseStep, BaseTraceFrame, NodeID, NodeType, TraceType } from './types';
+
+export interface Color {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+}
+
+export enum TextProperty {
+  COLOR = 'color',
+  ITALIC = 'italic',
+  UNDERLINE = 'underline',
+  FONT_WEIGHT = 'fontWeight',
+  FONT_FAMILY = 'fontFamily',
+  STRIKE_THROUGH = 'strikeThrough',
+}
+
+export enum ElementType {
+  LINK = 'link',
+}
+
+export enum ElementProperty {
+  TEXT_ALIGN = 'textAlign',
+}
+
+export interface Text extends BaseText {
+  [TextProperty.COLOR]?: Color;
+  [TextProperty.ITALIC]?: boolean;
+  [TextProperty.UNDERLINE]?: boolean;
+  [TextProperty.FONT_WEIGHT]?: string;
+  [TextProperty.FONT_FAMILY]?: string;
+  [TextProperty.STRIKE_THROUGH]?: boolean;
+}
+
+export interface Element extends BaseElement {
+  type?: ElementType;
+  [ElementProperty.TEXT_ALIGN]?: string;
+}
+
+export interface LinkElement extends Element {
+  type: ElementType.LINK;
+  url?: string;
+}
+
+export interface TextData {
+  id: string;
+  content: Array<Element | Text>;
+}
+
+export interface StepData {
+  texts: TextData[];
+  canvasVisibility?: CanvasNodeVisibility;
+}
+
+export interface Step extends BaseStep<StepData> {
+  type: NodeType.TEXT;
+}
+
+export interface TextNodeData {
+  id: string;
+  html: string;
+}
+
+export interface Node extends BaseNode {
+  type: NodeType.TEXT;
+  texts: TextNodeData[];
+  nextId?: NodeID;
+}
+
+export interface TraceFrame extends BaseTraceFrame<TextNodeData> {
+  type: TraceType.TEXT;
+}
