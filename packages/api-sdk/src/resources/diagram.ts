f02e7f2dc42194261303d7dd459e9176ebafc33d
Evgeny Zaytsev <overhawlin@gmail.com>
2021-08-18 18:16:28 +0300

    feat: improve api-sdk typings (#46)
    * feat: improve api-sdk typings
    
    * feat: allow fields be readonly array

--- packages/api-sdk/src/resources/diagram.ts
+++ packages/api-sdk/src/resources/diagram.ts
@@ -4,6 +4,7 @@ import type Fetch from '@/fetch';
 import { BaseDiagramNode, Diagram, DiagramID, NodeID, SDiagram, SNode, SNodeID, SNodePartial } from '@/models';
 import { createPutAndPostStruct } from '@/utils';
 
+import { Fields } from './base';
 import CrudResource from './crud';
 
 const ENDPOINT = 'diagrams';
@@ -26,15 +27,17 @@ class DiagramResource extends CrudResource<typeof SDiagram['schema'], ModelIDKey
     });
   }
 
-  public async get<T extends Partial<Diagram>>(id: DiagramID, fields: string[]): Promise<T>;
+  public async get<T extends Partial<Diagram>>(id: DiagramID, fields: Fields): Promise<T>;
 
   public async get<T extends BaseDiagramNode = BaseDiagramNode>(id: DiagramID): Promise<Diagram<T>>;
 
-  public async get(id: DiagramID, fields?: string[]) {
+  public async get<T extends Diagram<any> = Diagram>(id: DiagramID): Promise<T>;
+
+  public async get(id: DiagramID, fields?: Fields): Promise<Diagram<any>> {
     return fields ? super._getByID(id, fields) : super._getByID(id);
   }
 
-  public async getRTC<T extends Diagram>(id: DiagramID) {
+  public async getRTC<T extends Diagram<any> = Diagram>(id: DiagramID): Promise<{ diagram: T; timestamp: number }> {
     const { data } = await this.fetch.get<{ diagram: T; timestamp: number }>(`${this._getCRUDEndpoint(id)}/rtc`);
 
     return data;
@@ -42,30 +45,46 @@ class DiagramResource extends CrudResource<typeof SDiagram['schema'], ModelIDKey
 
   public async create<T extends BaseDiagramNode = BaseDiagramNode>(
     body: Omit<Diagram<T>, '_id'> & Partial<Pick<Diagram<T>, '_id'>>
-  ): Promise<Diagram<T>> {
-    return super._post<Diagram<T>>(body);
+  ): Promise<Diagram<T>>;
+
+  public async create<T extends Omit<Diagram<any>, '_id'>>(body: T): Promise<T & Pick<Diagram<any>, '_id'>>;
+
+  public async create(body: Diagram<any>): Promise<Diagram<any>> {
+    return super._post(body);
   }
 
-  public async update<T extends BaseDiagramNode = BaseDiagramNode>(id: DiagramID, body: Partial<Diagram<T>>): Promise<Partial<Diagram<T>>> {
-    return super._patch<Diagram<T>>(id, body);
+  public async update<T extends BaseDiagramNode = BaseDiagramNode>(id: DiagramID, body: Partial<Diagram<T>>): Promise<Partial<Diagram<T>>>;
+
+  public async update<T extends Partial<Diagram<any>>>(id: DiagramID, body: T): Promise<T>;
+
+  public async update(id: DiagramID, body: Partial<Diagram<any>>): Promise<Partial<Diagram<any>>> {
+    return super._patch(id, body);
   }
 
-  public async updateNode<T extends BaseDiagramNode>(id: DiagramID, nodeID: NodeID, body: Omit<T, 'nodeID'>): Promise<T> {
+  public async updateNode<T extends Omit<BaseDiagramNode, 'nodeID'>>(
+    id: DiagramID,
+    nodeID: NodeID,
+    body: T
+  ): Promise<T & Pick<BaseDiagramNode, 'nodeID'>> {
     this._assertModelID(id);
     s.assert(nodeID, SNodeID);
     s.assert(body, this._nodePutAndPostStruct);
 
-    const { data } = await this.fetch.put<T>(`${this._getCRUDEndpoint(id)}/nodes/${nodeID}`, body);
+    const { data } = await this.fetch.put<T & Pick<BaseDiagramNode, 'nodeID'>>(`${this._getCRUDEndpoint(id)}/nodes/${nodeID}`, body);
 
     return data;
   }
 
-  public async patchNode<T extends BaseDiagramNode>(id: DiagramID, nodeID: NodeID, body: Partial<Omit<T, 'nodeID'>>): Promise<T> {
+  public async patchNode<T extends Omit<BaseDiagramNode, 'nodeID'>>(
+    id: DiagramID,
+    nodeID: NodeID,
+    body: Partial<T>
+  ): Promise<T & Pick<BaseDiagramNode, 'nodeID'>> {
     this._assertModelID(id);
     s.assert(nodeID, SNodeID);
     s.assert(body, this._nodePatchStruct);
 
-    const { data } = await this.fetch.patch<T>(`${this._getCRUDEndpoint(id)}/nodes/${nodeID}`, body);
+    const { data } = await this.fetch.patch<T & Pick<BaseDiagramNode, 'nodeID'>>(`${this._getCRUDEndpoint(id)}/nodes/${nodeID}`, body);
 
     return data;
   }
--- packages/api-sdk/src/resources/program.ts
+++ packages/api-sdk/src/resources/program.ts
@@ -1,6 +1,7 @@
 import type Fetch from '@/fetch';
 import { BaseCommand, BaseNode, Program, ProgramID, SProgram } from '@/models';
 
+import { Fields } from './base';
 import CrudResource from './crud';
 
 const ENDPOINT = 'programs';
@@ -19,23 +20,33 @@ class ProgramResource extends CrudResource<typeof SProgram['schema'], ModelIDKey
     });
   }
 
-  public async get<T extends Partial<Program>>(id: ProgramID, fields: string[]): Promise<T>;
+  public async get<T extends Partial<Program>>(id: ProgramID, fields: Fields): Promise<T>;
 
   public async get<T extends BaseNode, C extends BaseCommand>(id: ProgramID): Promise<Program<T, C>>;
 
-  public async get(id: ProgramID, fields?: string[]) {
-    return fields ? super._getByID<Program>(id, fields) : super._getByID<Program>(id);
+  public async get<T extends Program<any, any> = Program>(id: ProgramID): Promise<T>;
+
+  public async get(id: ProgramID, fields?: Fields): Promise<Program<any, any>> {
+    return fields ? super._getByID(id, fields) : super._getByID(id);
   }
 
-  public async create<T extends BaseNode, C extends BaseCommand>(body: Omit<Program<T, C>, ModelIDKey>): Promise<Program<T, C>> {
-    return super._post<Program<T, C>>(body);
+  async create<T extends BaseNode, C extends BaseCommand>(body: Omit<Program<T, C>, ModelIDKey>): Promise<Program<T, C>>;
+
+  async create<T extends Omit<Program<any, any>, ModelIDKey>>(body: T): Promise<T & Pick<Program<any, any>, ModelIDKey>>;
+
+  public async create(body: Omit<Program<any, any>, ModelIDKey>): Promise<Program<any, any>> {
+    return super._post(body);
   }
 
   public async update<T extends BaseNode, C extends BaseCommand>(
     id: ProgramID,
     body: Omit<Program<T, C>, ModelIDKey>
-  ): Promise<Omit<Program<T, C>, ModelIDKey>> {
-    return super._put<Program<T, C>>(id, body);
+  ): Promise<Omit<Program<T, C>, ModelIDKey>>;
+
+  public async update<T extends Omit<Program<any, any>, ModelIDKey>>(id: ProgramID, body: T): Promise<T>;
+
+  public async update(id: ProgramID, body: Omit<Program<any, any>, ModelIDKey>): Promise<Omit<Program<any, any>, ModelIDKey>> {
+    return super._put<Program<any, any>>(id, body);
   }
 
   public async delete(id: ProgramID): Promise<ProgramID> {
