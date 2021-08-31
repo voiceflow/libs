921b1e31b2138f7193edd399d2231c10bf20c442
leartgjoni-voiceflow <leart@voiceflow.com>
2021-08-31 11:46:51 -0400

    fix: add agent name for df es member

--- packages/google-dfes-types/src/project/member.ts
+++ packages/google-dfes-types/src/project/member.ts
@@ -1,8 +1,10 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-
 import { Project } from '@voiceflow/google-types';
 
-export interface GoogleDFESProjectMemberData extends Project.BaseGoogleProjectMemberData {}
+import { Nullable } from '@/../../api-sdk/build';
+
+export interface GoogleDFESProjectMemberData extends Project.BaseGoogleProjectMemberData {
+  agentName: Nullable<string>;
+}
 
 export const defaultGoogleDFESProjectMemberData = (memberData: Partial<GoogleDFESProjectMemberData> = {}): GoogleDFESProjectMemberData => ({
   ...Project.defaultBaseGoogleProjectMemberData(memberData),
