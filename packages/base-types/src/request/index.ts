87fe5bcb168d96b46d1e589a59a3ef1dceb74594
Evgeny <overhawlin@gmail.com>
2021-09-10 12:24:58 +0200

    feat: add request label (VF-000)

--- packages/base-types/src/request/index.ts
+++ packages/base-types/src/request/index.ts
@@ -33,6 +33,9 @@ export interface Entity {
   verboseValue?: VerboseValue[]; // more detailed results from LUIS
 }
 
+export interface LabelRequestPayload {
+  label?: string;
+}
 export interface BaseRequest<P = unknown> {
   type: string;
   payload: P;
@@ -46,7 +49,7 @@ export interface TextRequest extends BaseRequest<string> {
   type: RequestType.TEXT;
 }
 
-export interface IntentRequestPayload extends ActionPayload {
+export interface IntentRequestPayload extends ActionPayload, LabelRequestPayload {
   query: string; // original text input
   intent: { name: string }; // matched intent name
   entities: Entity[]; // entities matches - multiple of the same entity name may be matched
@@ -57,11 +60,13 @@ export interface IntentRequest extends BaseRequest<IntentRequestPayload> {
   type: RequestType.INTENT;
 }
 
-export interface GeneralRequest extends BaseRequest<ActionPayload> {
+interface ActionAndLabelRequestPayload extends ActionPayload, LabelRequestPayload {}
+
+export interface GeneralRequest extends BaseRequest<ActionAndLabelRequestPayload> {
   type: string; // the general request type is dynamic, used to m
 }
 
-export interface ActionRequest extends BaseRequest<ActionPayload> {
+export interface ActionRequest extends BaseRequest<ActionAndLabelRequestPayload> {
   type: RequestType.ACTION;
 }
 
