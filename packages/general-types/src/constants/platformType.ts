975ba1d9e3ebc9f299397b4b0e6b15830dfa45de
Jonah Snider <jonah.snider@voiceflow.com>
2021-09-17 14:15:39 -0700

    feat(general-types): add PlatformType and TTSPlatformType (VF-1544) (#67)

---
+++ packages/general-types/src/constants/platformType.ts
@@ -0,0 +1,16 @@
+export enum PlatformType {
+  ALEXA = 'alexa',
+  GOOGLE = 'google',
+  GENERAL = 'general',
+  IVR = 'ivr',
+  CHATBOT = 'chatbot',
+  /** @deprecated will be removed soon™️ */
+  MOBILE_APP = 'mobile_app',
+  DIALOGFLOW = 'df-es',
+}
+
+export enum TTSPlatformType {
+  ALEXA = 'alexa',
+  AZURE = 'azure',
+  GOOGLE = 'google',
+}
