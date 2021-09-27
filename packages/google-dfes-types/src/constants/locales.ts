4e90280622c255d0f4eb00aa0ba103f61f386a1b
kerry918 <kerry.liu@voiceflow.com>
2021-09-27 18:18:31 -0400

    feat: update fields and locales for dialogflow (VF-1311)

--- packages/google-dfes-types/src/constants/locales.ts
+++ packages/google-dfes-types/src/constants/locales.ts
@@ -1,9 +1,10 @@
 export enum Language {
-  HK = 'zh-HK',
-  TW = 'zh-TW',
-  DA = 'da',
+  BN = 'bn',
+  ZH = 'zh',
   NL = 'nl',
   EN = 'en',
+  FIL = 'fil',
+  FI = 'fi',
   FR = 'fr',
   DE = 'de',
   HI = 'hi',
@@ -11,145 +12,172 @@ export enum Language {
   IT = 'it',
   JA = 'ja',
   KO = 'ko',
+  MS = 'ms',
+  MR = 'mr',
   NO = 'no',
   PL = 'pl',
   PT = 'pt',
+  RO = 'ro',
   RU = 'ru',
+  SI = 'si',
   ES = 'es',
   SV = 'sv',
+  TA = 'ta',
+  TE = 'te',
   TH = 'th',
   TR = 'tr',
+  UK = 'uk',
+  VI = 'vi',
 }
 
 export enum Locale {
+  // Bengali
+  BN = 'bn',
+
   // Chinese
   ZH_HK = 'zh-HK',
+  ZH_CN = 'zh-CN',
   ZH_TW = 'zh-TW',
-  // Danish
-  DA_DK = 'da-DK',
 
   // Dutch
-  NL_NL = 'nl-NL',
-  NL_BE = 'nl-BE',
+  NL = 'nl',
+
+  // Filipino
+  FIL = 'fil',
+
+  // Finnish
+  FI = 'fi',
 
   // English
-  EN_AU = 'en-AU',
-  EN_CA = 'en-CA',
-  EN_GB = 'en-GB',
-  EN_IN = 'en-IN',
-  EN_BE = 'en-BE',
-  EN_SG = 'en-SG',
-  EN_US = 'en-US',
+  EN = 'en',
 
   // French
-  FR_FR = 'fr-FR',
-  FR_CA = 'fr-CA',
-  FR_BE = 'fr-BE',
+  FR = 'fr',
 
   // German
-  DE_DE = 'de-DE',
-  DE_AT = 'de-AT',
-  DE_CH = 'de-CH',
-  DE_BE = 'de-BE',
+  DE = 'de',
 
   // Hindi
-  HI_IN = 'hi-IN',
+  HI = 'hi',
 
   // Indonasian
-  ID_ID = 'id-ID',
+  ID = 'id',
 
   // Italian
-  IT_IT = 'it-IT',
+  IT = 'it',
 
   // Japanese
-  JA_JP = 'ja-JP',
+  JA = 'ja',
 
   // Korean
-  KO_KR = 'ko-KR',
+  KO = 'ko',
+
+  // Malay
+  MS = 'ms',
+
+  // Marathi
+  MR = 'mr',
 
   // Norwegian
-  NO_NO = 'no-NO',
+  NO = 'no',
 
   // Polish
-  PL_PL = 'pl-PL',
+  PL = 'pl',
 
   // Portugese
   PT_BR = 'pt-BR',
+  PT = 'pt',
+
+  // Romanian
+  RO = 'ro',
 
   // Russian
-  RU_RU = 'ru-RU',
+  RU = 'ru',
+
+  // Sinhala
+  SI = 'si',
 
   // Spanish
-  ES_ES = 'es-ES',
-  ES_419 = 'es-419',
+  ES = 'es',
 
   // Swedish
-  SV_SE = 'sv-SE',
+  SV = 'sv',
+
+  // Tamil
+  TA = 'ta',
+
+  // Telugu
+  TE = 'te',
 
   // Thai
-  TH_TH = 'th-TH',
+  TH = 'th',
 
   // Turkish
-  TR_TR = 'tr-TR',
+  TR = 'tr',
+
+  // Ukrainian
+  UK = 'uk',
+
+  // Vietnamese
+  VI = 'vi',
 }
 
 export const LanguageToLocale: Record<Language, Locale[]> = {
-  [Language.DA]: [Locale.DA_DK],
-  [Language.DE]: [Locale.DE_DE, Locale.DE_AT, Locale.DE_CH, Locale.DE_BE],
-  [Language.EN]: [Locale.EN_US, Locale.EN_AU, Locale.EN_CA, Locale.EN_GB, Locale.EN_IN, Locale.EN_BE, Locale.EN_SG],
-  [Language.ES]: [Locale.ES_ES, Locale.ES_419],
-  [Language.FR]: [Locale.FR_FR, Locale.FR_CA, Locale.FR_BE],
-  [Language.HI]: [Locale.HI_IN],
-  [Language.HK]: [Locale.ZH_HK],
-  [Language.ID]: [Locale.ID_ID],
-  [Language.IT]: [Locale.IT_IT],
-  [Language.JA]: [Locale.JA_JP],
-  [Language.KO]: [Locale.KO_KR],
-  [Language.NL]: [Locale.NL_NL, Locale.NL_BE],
-  [Language.NO]: [Locale.NO_NO],
-  [Language.PL]: [Locale.PL_PL],
-  [Language.PT]: [Locale.PT_BR],
-  [Language.RU]: [Locale.RU_RU],
-  [Language.SV]: [Locale.SV_SE],
-  [Language.TH]: [Locale.TH_TH],
-  [Language.TR]: [Locale.TR_TR],
-  [Language.TW]: [Locale.ZH_TW],
+  [Language.BN]: [Locale.BN],
+  [Language.ZH]: [Locale.ZH_CN, Locale.ZH_HK, Locale.ZH_TW],
+  [Language.NL]: [Locale.NL],
+  [Language.EN]: [Locale.EN],
+  [Language.FIL]: [Locale.FIL],
+  [Language.FI]: [Locale.FI],
+  [Language.FR]: [Locale.FR],
+  [Language.DE]: [Locale.DE],
+  [Language.HI]: [Locale.HI],
+  [Language.ID]: [Locale.ID],
+  [Language.IT]: [Locale.IT],
+  [Language.JA]: [Locale.JA],
+  [Language.KO]: [Locale.KO],
+  [Language.MS]: [Locale.MS],
+  [Language.MR]: [Locale.MR],
+  [Language.NO]: [Locale.NO],
+  [Language.PL]: [Locale.PL],
+  [Language.PT]: [Locale.PT, Locale.PT_BR],
+  [Language.RO]: [Locale.RO],
+  [Language.RU]: [Locale.RU],
+  [Language.SI]: [Locale.SI],
+  [Language.ES]: [Locale.ES],
+  [Language.SV]: [Locale.SV],
+  [Language.TA]: [Locale.TA],
+  [Language.TE]: [Locale.TE],
+  [Language.TH]: [Locale.TH],
+  [Language.TR]: [Locale.TR],
+  [Language.UK]: [Locale.UK],
+  [Language.VI]: [Locale.VI],
 };
 
 export enum VoiceLanguageCode {
-  AR_XA = 'ar-XA',
   BN_IN = 'bn-IN',
+  YUE_HK = 'yue-HK',
   CMN_CN = 'cmn-CN',
   CMN_TW = 'cmn-TW',
-  CS_CZ = 'cs-CZ',
-  DA_DK = 'da-DK',
-  DE_DE = 'de-DE',
-  EL_GR = 'el-GR',
-  EN_AU = 'en-AU',
-  EN_GB = 'en-GB',
-  EN_IN = 'en-IN',
+  NL_NL = 'nl-NL',
   EN_US = 'en-US',
-  ES_ES = 'es-ES',
-  FI_FI = 'fi-FI',
   FIL_PH = 'fil-PH',
-  FR_CA = 'fr-CA',
+  FI_FI = 'fi-FI',
   FR_FR = 'fr-FR',
-  GU_IN = 'gu-IN',
+  DE_DE = 'de-DE',
   HI_IN = 'hi-IN',
-  HU_HU = 'hu-HU',
   ID_ID = 'id-ID',
   IT_IT = 'it-IT',
   JA_JP = 'ja-JP',
-  KN_IN = 'kn-IN',
   KO_KR = 'ko-KR',
-  ML_IN = 'ml-IN',
+  MS_MY = 'ms-MY',
   NB_NO = 'nb-NO',
-  NL_NL = 'nl-NL',
   PL_PL = 'pl-PL',
   PT_BR = 'pt-BR',
   PT_PT = 'pt-PT',
+  RO_RO = 'ro-RO',
   RU_RU = 'ru-RU',
-  SK_SK = 'sk-SK',
+  ES_ES = 'es-ES',
   SV_SE = 'sv-SE',
   TA_IN = 'ta-IN',
   TE_IN = 'te-IN',
@@ -157,60 +185,63 @@ export enum VoiceLanguageCode {
   TR_TR = 'tr-TR',
   UK_UA = 'uk-UA',
   VI_VN = 'vi-VN',
-  YUQ_HK = 'yue-HK',
 }
 
-export const LocaleToVoiceLanguageCode: Record<Locale, VoiceLanguageCode> = {
-  [Locale.DA_DK]: VoiceLanguageCode.DA_DK,
+export const LocaleToVoiceLanguageCode: { [keyof in Locale]?: VoiceLanguageCode } = {
+  [Locale.BN]: VoiceLanguageCode.BN_IN,
 
-  [Locale.DE_DE]: VoiceLanguageCode.DE_DE,
-  [Locale.DE_AT]: VoiceLanguageCode.DE_DE,
-  [Locale.DE_CH]: VoiceLanguageCode.DE_DE,
-  [Locale.DE_BE]: VoiceLanguageCode.DE_DE,
+  [Locale.ZH_HK]: VoiceLanguageCode.YUE_HK,
+  [Locale.ZH_CN]: VoiceLanguageCode.CMN_CN,
+  [Locale.ZH_TW]: VoiceLanguageCode.CMN_TW,
 
-  [Locale.NL_NL]: VoiceLanguageCode.NL_NL,
-  [Locale.NL_BE]: VoiceLanguageCode.NL_NL,
+  [Locale.NL]: VoiceLanguageCode.NL_NL,
 
-  [Locale.EN_US]: VoiceLanguageCode.EN_US,
-  [Locale.EN_CA]: VoiceLanguageCode.EN_US,
-  [Locale.EN_IN]: VoiceLanguageCode.EN_IN,
-  [Locale.EN_GB]: VoiceLanguageCode.EN_GB,
-  [Locale.EN_BE]: VoiceLanguageCode.EN_GB,
-  [Locale.EN_SG]: VoiceLanguageCode.EN_GB,
-  [Locale.EN_AU]: VoiceLanguageCode.EN_AU,
+  [Locale.EN]: VoiceLanguageCode.EN_US,
 
-  [Locale.ES_ES]: VoiceLanguageCode.ES_ES,
-  [Locale.ES_419]: VoiceLanguageCode.ES_ES,
+  [Locale.FIL]: VoiceLanguageCode.FIL_PH,
 
-  [Locale.FR_FR]: VoiceLanguageCode.FR_FR,
-  [Locale.FR_CA]: VoiceLanguageCode.FR_CA,
-  [Locale.FR_BE]: VoiceLanguageCode.FR_FR,
+  [Locale.FI]: VoiceLanguageCode.FI_FI,
 
-  [Locale.HI_IN]: VoiceLanguageCode.HI_IN,
+  [Locale.FR]: VoiceLanguageCode.FR_FR,
 
-  [Locale.ID_ID]: VoiceLanguageCode.ID_ID,
+  [Locale.DE]: VoiceLanguageCode.DE_DE,
 
-  [Locale.IT_IT]: VoiceLanguageCode.IT_IT,
+  [Locale.HI]: VoiceLanguageCode.HI_IN,
 
-  [Locale.JA_JP]: VoiceLanguageCode.JA_JP,
+  [Locale.ID]: VoiceLanguageCode.ID_ID,
 
-  [Locale.KO_KR]: VoiceLanguageCode.KO_KR,
+  [Locale.IT]: VoiceLanguageCode.IT_IT,
 
-  [Locale.NO_NO]: VoiceLanguageCode.NB_NO,
+  [Locale.JA]: VoiceLanguageCode.JA_JP,
 
-  [Locale.PL_PL]: VoiceLanguageCode.PL_PL,
+  [Locale.KO]: VoiceLanguageCode.KO_KR,
+
+  [Locale.MS]: VoiceLanguageCode.MS_MY,
+
+  [Locale.NO]: VoiceLanguageCode.NB_NO,
+
+  [Locale.PL]: VoiceLanguageCode.PL_PL,
+  [Locale.PT]: VoiceLanguageCode.PT_PT,
 
   [Locale.PT_BR]: VoiceLanguageCode.PT_BR,
 
-  [Locale.RU_RU]: VoiceLanguageCode.RU_RU,
+  [Locale.RO]: VoiceLanguageCode.RO_RO,
 
-  [Locale.SV_SE]: VoiceLanguageCode.SV_SE,
+  [Locale.RU]: VoiceLanguageCode.RU_RU,
 
-  [Locale.TH_TH]: VoiceLanguageCode.TH_TH,
+  [Locale.ES]: VoiceLanguageCode.ES_ES,
 
-  [Locale.TR_TR]: VoiceLanguageCode.TR_TR,
+  [Locale.SV]: VoiceLanguageCode.SV_SE,
 
-  [Locale.ZH_TW]: VoiceLanguageCode.CMN_TW,
+  [Locale.TA]: VoiceLanguageCode.TA_IN,
 
-  [Locale.ZH_HK]: VoiceLanguageCode.YUQ_HK,
+  [Locale.TE]: VoiceLanguageCode.TE_IN,
+
+  [Locale.TH]: VoiceLanguageCode.TH_TH,
+
+  [Locale.TR]: VoiceLanguageCode.TR_TR,
+
+  [Locale.UK]: VoiceLanguageCode.UK_UA,
+
+  [Locale.VI]: VoiceLanguageCode.VI_VN,
 };
