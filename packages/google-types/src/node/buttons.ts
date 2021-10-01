fa0602986ff47a5728056d519732f2205c9df22d
leartgjoni-voiceflow <leart@voiceflow.com>
2021-10-01 15:15:45 -0400

    chore: update type

--- packages/google-types/src/node/buttons.ts
+++ packages/google-types/src/node/buttons.ts
@@ -1,9 +1,10 @@
-import { Node } from '@voiceflow/base-types';
+import { Node as ChatNode } from '@voiceflow/chat-types';
+import { Node as GeneralNode } from '@voiceflow/general-types';
 
 export interface StepData {
   data: string;
 }
 
-export type Step = Node.Buttons.Step;
+export type Step = GeneralNode.Buttons.Step | ChatNode.Buttons.Step;
 
 export type ButtonNode = { name: string; nextID: string | null; type: 'PATH' | 'INTENT_PATH' } | { name: string; type: 'INTENT'; intentName: string };
