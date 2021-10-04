1eb8a0e8baa87ccee9639ed84eec20b8b73aace8
leartgjoni-voiceflow <leart@voiceflow.com>
2021-10-04 12:03:23 -0400

    fix: use enum

--- packages/google-types/src/node/buttons.ts
+++ packages/google-types/src/node/buttons.ts
@@ -7,4 +7,12 @@ export interface StepData {
 
 export type Step = GeneralNode.Buttons.Step | ChatNode.Buttons.Step;
 
-export type ButtonNode = { name: string; nextID: string | null; type: 'PATH' | 'INTENT_PATH' } | { name: string; type: 'INTENT'; intentName: string };
+export enum ButtonNodeType {
+  PATH = 'PATH',
+  INTENT_PATH = 'INTENT_PATH', // local intent - choice
+  INTENT = 'INTENT', // global intent
+}
+
+export type ButtonNode =
+  | { name: string; nextID: string | null; type: ButtonNodeType.PATH | ButtonNodeType.INTENT_PATH }
+  | { name: string; type: ButtonNodeType.INTENT; intentName: string };
