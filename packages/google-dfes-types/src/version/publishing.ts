cc8b6ec0c5b56bb24434021e8415815cefd50cc1
kerry918 <kerry.liu@voiceflow.com>
2021-10-22 15:23:39 -0400

    feat: add triggerPhrase property to publishing for dfes

--- packages/google-dfes-types/src/version/publishing.ts
+++ packages/google-dfes-types/src/version/publishing.ts
@@ -4,12 +4,15 @@ import { Locale } from '@/constants';
 
 export interface GoogleDFESVersionPublishing extends Version.BaseGoogleVersionPublishing {
   locales: Locale[];
+  triggerPhrase?: string;
 }
 
 export const defaultGoogleDFESVersionPublishing = ({
   locales = [],
+  triggerPhrase,
   ...baseGoogleVersionPublising
 }: Partial<GoogleDFESVersionPublishing> = {}): GoogleDFESVersionPublishing => ({
   ...Version.defaultBaseGoogleVersionPublishing(baseGoogleVersionPublising),
   locales,
+  triggerPhrase,
 });
