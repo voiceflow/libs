6d217d6aaff2d26903238d32419fd4dd72bed6f1
Matheus Poleza <matheus.poleza@gmail.com>
2021-10-28 11:09:29 -0300

    feat(google-dfes-types): adds agent name to publishing data (VF-2118) (#110)
    Co-authored-by: Matheus Poleza <matheus.poleza@voiceflow.com>

--- packages/google-dfes-types/src/version/publishing.ts
+++ packages/google-dfes-types/src/version/publishing.ts
@@ -5,14 +5,17 @@ import { Locale } from '@/constants';
 export interface GoogleDFESVersionPublishing extends Version.BaseGoogleVersionPublishing {
   locales: Locale[];
   triggerPhrase?: string[];
+  agentName?: string;
 }
 
 export const defaultGoogleDFESVersionPublishing = ({
   locales = [],
   triggerPhrase = [],
+  agentName,
   ...baseGoogleVersionPublising
 }: Partial<GoogleDFESVersionPublishing> = {}): GoogleDFESVersionPublishing => ({
   ...Version.defaultBaseGoogleVersionPublishing(baseGoogleVersionPublising),
   locales,
   triggerPhrase,
+  agentName,
 });
