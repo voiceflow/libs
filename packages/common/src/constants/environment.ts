532beadc8e27b51817d41a8ccb8a23151c6442b0
xavidop <xavi_tb@hotmail.com>
2021-11-16 15:50:47 +0100

    ci: push changes 7.7.1 common

--- packages/common/src/constants/environment.ts
+++ packages/common/src/constants/environment.ts
@@ -4,5 +4,6 @@ export enum Environment {
   LOCAL = 'local',
   E2E = 'e2e',
   TEST = 'test',
+  /** @deprecated Voiceflow dev envs now use {@link Environment.DEVELOPMENT}  */
   STAGING = 'staging',
 }
