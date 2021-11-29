37a1fedba72cf8939db9edfe62bb210fe8845261
Josh <josh@voiceflow.com>
2021-11-29 15:03:06 -0500

    feat: transcript message tpye (VF-000)

--- packages/base-types/src/models/transcripts.ts
+++ packages/base-types/src/models/transcripts.ts
@@ -12,3 +12,22 @@ export interface Transcript {
   creatorID: CreatorID;
   projectID: ProjectID;
 }
+
+
+export enum FormatType {
+  Trace = 'trace',
+  Request = 'request',
+}
+
+export interface TranscriptMessage {
+  ingested_at: string;
+  metadata: { end: boolean; locale: string };
+  session_id: string;
+  state: unknown;
+  timestamp: string;
+  turn_id: string;
+  type: string;
+  version_id: string;
+  format: string;
+  payload: unknown;
+}
