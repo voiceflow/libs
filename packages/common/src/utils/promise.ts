71a5060592e4cf0366abd1e595ce490cfaa628d8
Jonah Snider <jonah@jonahsnider.com>
2022-01-13 12:31:16 -0800

    refactor: comply with no-promise-executor-return rule

--- packages/common/src/utils/promise.ts
+++ packages/common/src/utils/promise.ts
@@ -1,8 +1,14 @@
-export const delay = <T = boolean>(timeout: number, value: T = true as T): Promise<T> =>
-  new Promise<T>((resolve) => setTimeout(resolve.bind(null, value), timeout));
+export const delay = <T = boolean>(timeout: number, value: T = true as unknown as T): Promise<T> =>
+  new Promise<T>((resolve) => {
+    setTimeout(() => resolve(value), timeout);
+  });
 
 export const rejectIn = (timeout: number): Promise<never> =>
-  new Promise<never>((_resolve, reject) => setTimeout(() => reject(new Error('Rejected by timeout!')), timeout));
+  new Promise<never>((_resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Rejected by timeout!'));
+    }, timeout);
+  });
 
 interface ControlledPromise<T> extends Promise<T> {
   reject: (reason?: any) => void;
