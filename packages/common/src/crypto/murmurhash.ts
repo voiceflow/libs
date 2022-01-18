8a261132af26e7e488423ab8fec52b86549cbce5
David Lu <david.lu@voiceflow.com>
2022-01-18 15:46:41 -0500

    feat: added a murmurhash3 hash method to common (VF-2617)

---
+++ packages/common/src/crypto/murmurhash.ts
@@ -0,0 +1,11 @@
+import { MurmurHash3 } from 'murmurhash-wasm';
+
+class MurmurHash {
+  public static hash(key: string, seed: number): string {
+    const hash = MurmurHash3.hash32(key, seed);
+    const value = hash.readUInt32BE();
+    return value.toString(16);
+  }
+}
+
+export default MurmurHash;
