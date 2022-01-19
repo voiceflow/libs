420349ffff31ab1da48389a489f1a3c0771adc79
David Lu <david.lu@voiceflow.com>
2022-01-19 15:56:13 -0500

    feat: changed tests

--- packages/common/tests/crypto/murmuhash.unit.ts
+++ packages/common/tests/crypto/murmuhash.unit.ts
@@ -12,13 +12,13 @@ import { MurmurHash } from '@/crypto/murmurhash';
 
 describe('hash()', () => {
   it('return a 32-bit hash resulting from the murmurhash3 algorithm', () => {
-    expect(MurmurHash.hash('hello', 0)).to.eql('FIEEdaEG');
-    expect(MurmurHash.hash('hello world', 0)).to.eql('EEeBEJAF');
-    expect(MurmurHash.hash('helloworld', 0)).to.eql('BIFECFfG');
-    expect(MurmurHash.hash('HELLOWORLD', 0)).to.eql('dIBEIJda');
-    expect(MurmurHash.hash('', 0)).to.eql('A'); // not sure why '0000 0000' fails as a test
-    expect(MurmurHash.hash('abc', 0)).to.eql('bDddJDfa');
-    expect(MurmurHash.hash('ABC', 0)).to.eql('EDcBcFDF');
+    expect(MurmurHash.hash('hello', 0)).to.eql('lokkdakm');
+    expect(MurmurHash.hash('hello world', 0)).to.eql('kkehkpgl');
+    expect(MurmurHash.hash('helloworld', 0)).to.eql('holkilfm');
+    expect(MurmurHash.hash('HELLOWORLD', 0)).to.eql('dohkopda');
+    expect(MurmurHash.hash('', 0)).to.eql('g'); // not sure why '0000 0000' fails as a test
+    expect(MurmurHash.hash('abc', 0)).to.eql('bjddpjfa');
+    expect(MurmurHash.hash('ABC', 0)).to.eql('kjchcljl');
 
     // from https://github.com/jonahsnider/murmurhash-wasm/blob/main/test/murmur-hash3/hash-32.test.ts
     // expect(MurmurHash.hash(buf_left.toString('hex'), 0)).to.eql(buf_right.toString('hex'));
