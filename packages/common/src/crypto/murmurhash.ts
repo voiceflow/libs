4d67f6f2b5101fc99157f1a8d91789fdfe0d2d83
David Lu <david.lu@voiceflow.com>
2022-01-19 13:40:03 -0500

    feat: fixed hashcode to only contain alphabetical characters, added and modified tests

--- packages/common/src/crypto/murmurhash.ts
+++ packages/common/src/crypto/murmurhash.ts
@@ -4,7 +4,20 @@ export class MurmurHash {
   public static hash(key: string, seed: number): string {
     const hash = MurmurHash3.hash32(key, seed);
     const value = hash.readUInt32BE();
-    return value.toString(16);
+    const hex = value.toString(16);
+
+    let ret = ''; // store the return value
+
+    for (let i = 0; i < hex.length - 1; i++) {
+      // loop through the hex value and convert numbers to alphabet
+      let sub = hex.slice(i, i + 1);
+      if (sub.charCodeAt(0) > 47 && sub.charCodeAt(0) < 58) {
+        // if the character is a number
+        sub = String.fromCharCode(sub.charCodeAt(0) + 17); // add 17 to the character code (convert to alphabet)
+      }
+      ret += sub; // append the converted character to the return value
+    }
+    return ret;
   }
 }
 
