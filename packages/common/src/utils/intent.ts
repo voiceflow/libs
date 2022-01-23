dca51919f0bb48ee827d319f0aadab76f16c04b1
Tyler Han <tylerhan97@gmail.com>
2022-01-23 16:36:44 -0500

    feat: utterance permutations (VF-000)

--- packages/common/src/utils/intent.ts
+++ packages/common/src/utils/intent.ts
@@ -1,3 +1,5 @@
+import _sample from 'lodash/sample';
+
 import { BuiltinSlot, LOWER_CASE_CUSTOM_SLOT_TYPE, SLOT_REGEXP, SPACE_REGEXP } from '@/constants';
 
 export const formatIntentName = (name: string): string => {
@@ -46,4 +48,103 @@ export const getSlotType = (slots: BuiltinSlot<string, string>[], slot: { name: 
   }
 
   return type;
+};
+
+export const VF_ENTITY_REGEXP = /{{\[(\w{1,32})]\.(\w{1,32})}}/gi;
+const VF_ENTITY_REGEXP_LOCAL = new RegExp(VF_ENTITY_REGEXP, '');
+
+export interface JSONEntity {
+  startPos: number;
+  endPos: number;
+  entity: string;
+}
+export interface JSONUtterance {
+  text?: string;
+  entities?: JSONEntity[];
+}
+
+export const utteranceEntityPermutations = (
+  utterances: string[],
+  entitiesByID: Record<string, { inputs: string[]; name: string }>,
+  limit = 22
+  // eslint-disable-next-line sonarjs/cognitive-complexity
+): JSONUtterance[] => {
+  const newUtterances: JSONUtterance[] = [];
+  const unusedEntities: Record<string, { utterances: string[]; samples: string[] }> = {};
+
+  const addNewUtterance = (utterance: string) => {
+    if (!utterance?.trim()) return;
+
+    const entities: JSONEntity[] = [];
+
+    // Find all occurences of {entityName} in training utterances
+    let temp: string | undefined;
+    let current = utterance;
+
+    // keep replacing until there is nothing to replace (local replaces one instance at a time, this is important to keep positional data)
+    while (temp !== current) {
+      temp = utterance;
+      current = current.replace(VF_ENTITY_REGEXP_LOCAL, (_match: string, entityName: string, entityID: string, offset: number) => {
+        const entity = entitiesByID[entityID];
+        if (!entity) return entityName;
+
+        const sample = (unusedEntities[entityID]?.samples.shift() || _sample(entity?.inputs?.join(',')?.split(',')) || entityName).trim();
+        if (!unusedEntities[entityID]?.samples?.length) delete unusedEntities[entityID];
+
+        // This module should additionally create one full training utterance with positional entity (startPos, endPos, entityName).
+        const startPos = offset || 0;
+        const endPos = startPos + sample.length - 1;
+        entities.push({
+          startPos,
+          endPos,
+          entity: entity.name,
+        });
+
+        // Replace the entities with the sample value
+        return sample;
+      });
+    }
+
+    newUtterances.push({
+      text: utterance,
+      entities,
+    });
+  };
+
+  // try to ensure every single entity utterance is implemented within an intent
+  let i = 0;
+
+  // find all the entities referenced by this intent
+  // first pass over all utterances guarantees every utterance used
+  utterances.forEach((utterance) => {
+    // find all the entities used in each utterance
+    const entityMatches = [...utterance.matchAll(VF_ENTITY_REGEXP)];
+    entityMatches.forEach((match) => {
+      const entityID = match[2];
+      // if this entity hasn't been visited before, create samples for it
+      if (!unusedEntities[entityID]) {
+        unusedEntities[entityID] = { samples: [], utterances: [] };
+        const entity = entitiesByID[entityID];
+        if (entity) {
+          entity.inputs.forEach((input) => input.split(',').forEach((sample) => unusedEntities[entityID].samples.push(sample)));
+        }
+      }
+
+      unusedEntities[entityID].utterances.push(utterance);
+    });
+
+    addNewUtterance(utterance);
+    i++;
+  });
+
+  while (Object.keys(unusedEntities).length > 0 && i < limit) {
+    const entityID = Object.keys(unusedEntities)[0];
+    const _utterances = unusedEntities[entityID]?.utterances;
+    if (!_utterances) delete unusedEntities[entityID];
+
+    addNewUtterance(_utterances[i % _utterances.length]);
+    i++;
+  }
+
+  return newUtterances;
 };
