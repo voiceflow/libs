9be6b0596ed3dc26e9cb099411a505c49eba2279
Tyler Han <tylerhan97@gmail.com>
2022-02-03 14:06:24 -0500

    chore: prompt typeguard

--- packages/voiceflow-types/src/node/prompt/index.ts
+++ packages/voiceflow-types/src/node/prompt/index.ts
@@ -1,3 +1,8 @@
+import { ChatModels } from '@voiceflow/chat-types';
+import { VoiceModels } from '@voiceflow/voice-types';
+
+import { Voice } from '@/constants';
+
 import { ChatStep, ChatStepData } from './chat';
 import { VoiceStep, VoiceStepData } from './voice';
 
@@ -6,3 +11,14 @@ export * from './voice';
 
 export type Step = ChatStep | VoiceStep;
 export type StepData = ChatStepData | VoiceStepData;
+
+export type AnyPrompt = VoiceModels.Prompt<Voice> | ChatModels.Prompt | VoiceModels.IntentPrompt<string>;
+
+export const isChatPrompt = (prompt?: AnyPrompt | null): prompt is ChatModels.Prompt =>
+  !!(prompt && 'content' in prompt && Array.isArray(prompt.content));
+
+export const isVoicePrompt = (prompt?: AnyPrompt | null): prompt is VoiceModels.Prompt<Voice> =>
+  !!(prompt && 'content' in prompt && typeof prompt.content === 'string');
+
+export const isIntentVoicePrompt = (prompt?: AnyPrompt | null): prompt is VoiceModels.IntentPrompt<Voice> =>
+  !!(prompt && 'text' in prompt && typeof prompt.text === 'string');
