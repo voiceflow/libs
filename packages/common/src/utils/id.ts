c9884b96b6a24af07eff8e1c7fb7f877846dbb24
Tyler Han <tylerhan97@gmail.com>
2022-02-11 18:05:55 -0500

    fix: remap object id strings (VF-2914) (#192)
    * fix: remap object id strings (VF-2914)
    
    * test: add test case for object keys
    
    * feat: use deep object mapping instead of operating on JSON string
    
    * fix: remove unused parameter
    
    * chore: more concise implementation
    
    Co-authored-by: Jonah Snider <jonah@jonahsnider.com>

--- packages/common/src/utils/id.ts
+++ packages/common/src/utils/id.ts
@@ -1,6 +1,23 @@
 import ObjectId from 'bson-objectid';
 import cuid from 'cuid';
+import deepMap from 'deep-map';
+import deepMapKeys from 'deep-map-keys';
+
+import { EmptyObject } from '../types';
 
 export { cuid };
 
 export const objectID = (): string => new ObjectId().toHexString();
+
+export const remapObjectIDs = <T extends Readonly<EmptyObject>>(object: T, lookupMap: Record<string, string> | Map<string, string>): T => {
+  const map: ReadonlyMap<string, string> = lookupMap instanceof Map ? lookupMap : new Map(Object.entries(lookupMap));
+
+  const mapping = (value: any) => {
+    if (typeof value === 'string' && map.has(value)) {
+      return map.get(value)!;
+    }
+    return value;
+  };
+
+  return deepMapKeys(deepMap(object, mapping), mapping);
+};
