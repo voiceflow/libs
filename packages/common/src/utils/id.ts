0c5965e1844262e8abf3abdc5e3251ce8675e044
Jonah Snider <jonah@jonahsnider.com>
2022-02-11 14:19:00 -0800

    feat: use deep object mapping instead of operating on JSON string

--- packages/common/src/utils/id.ts
+++ packages/common/src/utils/id.ts
@@ -1,12 +1,36 @@
 import ObjectId from 'bson-objectid';
 import cuid from 'cuid';
+import deepMap from 'deep-map';
+import deepMapKeys from 'deep-map-keys';
+
+import { EmptyObject } from '../types';
 
 export { cuid };
 
 export const objectID = (): string => new ObjectId().toHexString();
 
-export const OBJECT_ID_REGEX = /"([\dA-Fa-f]{24})"/g;
+export const remapObjectIDs = <T extends Readonly<EmptyObject>>(object: T, lookupMap: Record<string, string> | Map<string, string>): T => {
+  const map: ReadonlyMap<string, string> = lookupMap instanceof Map ? lookupMap : new Map(Object.entries(lookupMap));
 
-// please feel free to suggest more performant implementations
-export const remapObjectIDs = <A>(object: A, map: Record<string, string>): A =>
-  JSON.parse(JSON.stringify(object).replace(OBJECT_ID_REGEX, (_, match) => `"${map[match] ?? match}"`));
+  const withUpdatedValues = deepMap(object, (value, key) => {
+    let newValue = value;
+
+    if (typeof value === 'string' && map.has(value)) {
+      newValue = map.get(value)!;
+    }
+
+    return newValue;
+  });
+
+  const withUpdatedKeys = deepMapKeys(withUpdatedValues, (key) => {
+    let newKey = key;
+
+    if (typeof key === 'string' && map.has(key)) {
+      newKey = map.get(key)!;
+    }
+
+    return newKey;
+  });
+
+  return withUpdatedKeys as T;
+};
