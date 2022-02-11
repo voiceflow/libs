5ac2d34222450fa68c54a56e7dacc89c79d31cf7
Jonah Snider <jonah@jonahsnider.com>
2022-02-11 14:17:23 -0800

    test: add test case for object keys

--- packages/common/tests/utils/id.unit.ts
+++ packages/common/tests/utils/id.unit.ts
@@ -27,5 +27,39 @@ describe('Utils | id', () => {
 
       expect(remapObjectIDs({ 1: { 2: { 3: Sample.A, 4: [Sample.REAL] } } }, map)).to.eql({ 1: { 2: { 3: Sample.B, 4: [Sample.C] } } });
     });
+
+    it('converts very nested', () => {
+      const object = {
+        a: { b: { c: { d: { e: 'id1' } } } },
+        id1: {
+          id2: {
+            id3: {
+              id4: ['id5'],
+            },
+          },
+          b: 'id2',
+        },
+      };
+
+      const map = {
+        id1: 'new1',
+        id2: 'new2',
+        id3: 'new3',
+        id4: 'new4',
+        id5: 'new5',
+      };
+
+      expect(remapObjectIDs(object, map)).to.eql({
+        a: { b: { c: { d: { e: 'new1' } } } },
+        new1: {
+          new2: {
+            new3: {
+              new4: ['new5'],
+            },
+          },
+          b: 'new2',
+        },
+      });
+    });
   });
 });
