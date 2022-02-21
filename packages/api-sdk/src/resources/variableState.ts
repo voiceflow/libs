a96c3a2ab24d52a98fc38003408e9ac212b3ade3
Vinicius Dacal <viniciusldacal@gmail.com>
2022-02-21 13:54:14 -0300

    feat: add variableState resource to api-sdk (VF-2609)

---
+++ packages/api-sdk/src/resources/variableState.ts
@@ -0,0 +1,27 @@
+import Fetch from '@api-sdk/fetch';
+import { BaseModels } from '@voiceflow/base-types';
+
+import { Fields } from './base';
+import CrudResource from './crud';
+
+export const ENDPOINT = 'variable-states';
+
+export type ModelKey = '_id';
+
+class VariableStateResource extends CrudResource<BaseModels.VariableState.Model, ModelKey, VariableStateResource, 'projectID' | 'name'> {
+  constructor(fetch: Fetch) {
+    super({
+      fetch,
+      clazz: VariableStateResource,
+      endpoint: ENDPOINT,
+    });
+  }
+
+  public async get<T extends Partial<BaseModels.VariableState.Model>>(id: string, fields: Fields): Promise<T>;
+
+  public async get(id: string, fields?: Fields): Promise<BaseModels.VariableState.Model> {
+    return fields ? super._getByID(id, fields) : super._getByID(id);
+  }
+}
+
+export default VariableStateResource;
