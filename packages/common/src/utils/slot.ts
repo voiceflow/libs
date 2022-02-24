fb3f1cebaacb062164d4286d1d65f8ffea24eb18
Jonah Snider <jonah@jonahsnider.com>
2022-02-24 13:55:16 -0800

    feat(common): move addPrebuiltSamples function from luis-authoring-service

--- packages/common/src/utils/slot.ts
+++ packages/common/src/utils/slot.ts
@@ -1,3 +1,5 @@
+import { BaseModels } from '@voiceflow/base-types';
+import { VoiceflowConstants } from '@voiceflow/voiceflow-types';
 import _uniqBy from 'lodash/uniqBy';
 
 export const addPrebuiltEntities = <A extends { key: string; inputs: string[] }>(entities: A[], prebuiltEntities: Record<string, string[]>): A[] =>
@@ -15,3 +17,15 @@ export const getUniqueSamples = (input: string) => _uniqBy(input.split(','), (sa
 
 // spread all synonyms into string array ['car, automobile', 'plane, jet'] => ['car', 'automobile', 'plane', 'jet']
 export const getAllSamples = (inputs: string[] = []) => inputs.flatMap((input) => input.split(',')).filter((sample) => !!sample.trim());
+
+export const addPrebuiltSamples = (slot: BaseModels.Slot, language: string = VoiceflowConstants.Language.EN) => {
+  const SLOT_SAMPLES = VoiceflowConstants.SlotTypes[language] || VoiceflowConstants.SlotTypes[VoiceflowConstants.Language.EN];
+
+  const slotSample = SLOT_SAMPLES.find((s) => s.name === slot.type.value);
+  return slotSample
+    ? {
+        ...slot,
+        inputs: slot.inputs.concat(slotSample.values),
+      }
+    : slot;
+};
