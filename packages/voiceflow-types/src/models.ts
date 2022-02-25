d9daa0892447c91ff1e6e2c158d135b5e49c944a
Tyler Han <tyler@voiceflow.com>
2022-02-25 20:00:00 +0000

    fix: update vf file types (VF-000) (#207)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-000**
    
    ### Brief description. What is this change?
    
    Based on feedback from replicant.
    
    
    Co-authored-by: Tyler Han <tylerhan97@gmail.com>

--- packages/voiceflow-types/src/models.ts
+++ packages/voiceflow-types/src/models.ts
@@ -1,9 +1,17 @@
 import { Diagram } from '@voiceflow-types/diagram';
+import { Program } from '@voiceflow-types/program';
 import { Project } from '@voiceflow-types/project';
 import { Version } from '@voiceflow-types/version';
 
 export interface VF {
   project: Project;
   version: Version;
-  diagrams: Diagram[];
+  diagrams: Record<string, Diagram>;
+}
+
+export interface VFR {
+  project: Project;
+  version: Version;
+  diagrams: Record<string, Diagram>;
+  programs: Record<string, Program>;
 }
