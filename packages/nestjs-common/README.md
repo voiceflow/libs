fe03a50411714d0fba03a2f1fb9e1ec86912fdd0
Jonah Snider <jonah@jonahsnider.com>
2022-03-07 22:26:38 +0000

    feat(nestjs-common): create `@voiceflow/nestjs-common` package (VF-2948) (#218)
    **Fixes or implements VF-2948**
    
    ### Brief description. What is this change?
    
    a library with reusable utilities for NestJS apps & libraries
    
    ### Checklist
    
    - [ ] this is a breaking change and should publish a new major version
    - [x] appropriate tests have been written

---
+++ packages/nestjs-common/README.md
@@ -0,0 +1,9 @@
+# NestJS Common
+
+Reusable utilities for NestJS apps & libraries.
+
+## Installation
+
+```sh
+yarn add @voiceflow/nestjs-common
+```
---
+++ packages/nestjs-common/tests/health/health-check-error.dto.unit.ts
@@ -0,0 +1,28 @@
+import { HealthCheckError } from '@nestjs-common/health/health-check-error.dto';
+import { expect } from 'chai';
+
+describe('HealthCheckError', () => {
+  it('works with Error instances', () => {
+    const error = new Error('test');
+    const healthCheckError = new HealthCheckError(error);
+
+    expect(healthCheckError.error).to.deep.equal({
+      name: 'Error',
+      message: 'test',
+      stack: error.stack,
+    });
+    expect(healthCheckError.rawError).to.eq(error);
+  });
+
+  it('works with other objects', () => {
+    const error = {
+      key: 'value',
+    };
+    const healthCheckError = new HealthCheckError(error);
+
+    expect(healthCheckError.error).to.eq(null);
+    expect(healthCheckError.rawError).to.eq(error);
+    // This tests typesafety of the generic
+    expect(healthCheckError.rawError.key).to.eq(error.key);
+  });
+});
