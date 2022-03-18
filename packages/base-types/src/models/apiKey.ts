d59cce982a004711a8a0990b1b8e6704b9ea7e28
Tyler Stewart <hello@tylerstewart.ca>
2022-03-18 18:20:00 +0000

    feat: expose api key types and guards (VF-000) (#226)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Adds api key metadata types and guards for use in other packages.
    
    Co-authored-by: Tyler Stewart <tyler.stewart@voiceflow.com>

--- packages/base-types/src/models/apiKey.ts
+++ packages/base-types/src/models/apiKey.ts
@@ -1,11 +1,28 @@
 import { AnyRecord } from '@base-types/types';
 
+export const API_KEY_PREFIX = 'VF.';
+
+export enum APIKeySubType {
+  Workspace = 'WS',
+  DialogManager = 'DM',
+}
+
+const buildAPIKeyGuard =
+  (type: APIKeySubType) =>
+  (key: unknown): key is string =>
+    typeof key === 'string' && key.startsWith(`${API_KEY_PREFIX}${type}.`);
+
+export const isWorkspaceAPIKey = buildAPIKeyGuard(APIKeySubType.Workspace);
+
+export const isDialogManagerAPIKey = buildAPIKeyGuard(APIKeySubType.DialogManager);
+
 export interface Model<Data extends AnyRecord = AnyRecord> {
   _id: string;
   creatorID: number;
   projectID?: string;
   workspaceID: string;
 
+  type: APIKeySubType;
   name: string;
   data?: Data;
   scopes: string[];
