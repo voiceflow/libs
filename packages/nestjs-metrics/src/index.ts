f6542b665345ef6958f2b7f942e510662b775065
Jonah Snider <jonah@jonahsnider.com>
2022-03-24 20:31:30 +0000

    fix(nestjs-metrics): export everything (VF-000) (#229)
    **Fixes or implements VF-000**
    
    ### Brief description. What is this change?
    
    forgot to export some of the types & values used for nestjs DI and module registration
    
    ### Checklist
    
    - [ ] this is a breaking change and should publish a new major version
    - [ ] appropriate tests have been written

--- packages/nestjs-metrics/src/index.ts
+++ packages/nestjs-metrics/src/index.ts
@@ -1,3 +1,5 @@
+export { Providers } from './constants';
+export { MetricsModuleAsyncOptions, MetricsOptions } from './interfaces/metrics-options.interface';
 export { MetricsModule } from './metrics.module';
 export { MetricsService } from './metrics.service';
 export { HistogramStopwatch } from './stopwatch';
