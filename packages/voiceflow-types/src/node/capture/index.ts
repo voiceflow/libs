3413548a973f242e5cbe08a0b0e199e864ce6b48
Ben Teichman <ben@voiceflow.com>
2022-04-01 12:57:04 -0400

    fix: expose more types

--- packages/voiceflow-types/src/node/capture/index.ts
+++ packages/voiceflow-types/src/node/capture/index.ts
@@ -1,3 +1,5 @@
+import { BaseNode } from '@voiceflow/base-types';
+
 import { ChatNode, ChatStep, ChatStepData } from './chat';
 import { VoiceNode, VoiceStep, VoiceStepData } from './voice';
 
@@ -6,6 +8,9 @@ export * from './voice';
 
 /** @deprecated */
 export type Step = ChatStep | VoiceStep;
+
+/** @deprecated */
+export type StepPorts = BaseNode.Capture.StepPorts;
 
 /** @deprecated */
 export type StepData = ChatStepData | VoiceStepData;
