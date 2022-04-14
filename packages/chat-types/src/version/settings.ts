8faef67912d8c71be62735bf2842be5431d31ad1
Josh <josh@voiceflow.com>
2022-04-14 03:22:21 +0000

    fix: reduce message delay default (VF-000) (#240)

--- packages/chat-types/src/version/settings.ts
+++ packages/chat-types/src/version/settings.ts
@@ -7,7 +7,7 @@ export interface Settings extends BaseVersion.Settings<Prompt> {
   messageDelay?: BaseModels.Version.PrototypeMessageDelay;
 }
 
-export const defaultMessageDelay = ({ durationMilliseconds = 1500 }: Partial<BaseModels.Version.PrototypeMessageDelay> = {}) => ({
+export const defaultMessageDelay = ({ durationMilliseconds = 1000 }: Partial<BaseModels.Version.PrototypeMessageDelay> = {}) => ({
   durationMilliseconds,
 });
 
