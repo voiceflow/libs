0bc97aba001669df4c98d597901a4b45df302df3
Tyler Han <tyler@voiceflow.com>
2022-04-22 22:08:06 -0400

    chore: visual step data

--- packages/google-dfes-types/src/node/visual.ts
+++ packages/google-dfes-types/src/node/visual.ts
@@ -1,7 +1,7 @@
 import { BaseNode } from '@voiceflow/base-types';
 
-export interface Step extends BaseNode.Visual.Step<BaseNode.Visual.ImageStepData> {}
+export interface Step extends BaseNode.Visual.Step<BaseNode.Visual.StepData> {}
 
 export interface Node extends BaseNode.Visual.Node {
-  data: BaseNode.Visual.ImageStepData;
+  data: BaseNode.Visual.StepData;
 }
