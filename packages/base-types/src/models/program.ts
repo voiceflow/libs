2d867fd2ff2b7cec3057f78922f9066a60f2f273
Tyler Han <tyler@voiceflow.com>
2022-05-04 16:57:51 -0400

    fix: program versionID (VF-3461)

--- packages/base-types/src/models/program.ts
+++ packages/base-types/src/models/program.ts
@@ -3,7 +3,11 @@ import { BaseCommand, BaseNode, Variable } from './base';
 export interface Model<Node extends BaseNode = BaseNode, Command extends BaseCommand = BaseCommand> {
   id: string;
   startId: string;
+
+  /** @deprecated in favor of versionID */
   skill_id: string;
+
+  versionID: string;
 
   name?: string;
   lines: Record<string, Node>;
