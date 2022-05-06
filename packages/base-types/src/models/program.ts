e2c1a6d57a4bf3c00e4bf24d6e9ed0ef37bbe20b
Tyler Han <tyler@voiceflow.com>
2022-05-06 14:49:40 -0400

    feat: use versionID (VF-3461)

--- packages/base-types/src/models/program.ts
+++ packages/base-types/src/models/program.ts
@@ -3,10 +3,6 @@ import { BaseCommand, BaseNode, Variable } from './base';
 export interface Model<Node extends BaseNode = BaseNode, Command extends BaseCommand = BaseCommand> {
   id: string;
   startId: string;
-
-  /** @deprecated in favor of versionID */
-  skill_id: string;
-
   versionID: string;
 
   name?: string;
