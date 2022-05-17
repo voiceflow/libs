c19e3aab2fc7285ddd0c89a826f3d91048f7854f
Evgeny <overhawlin@gmail.com>
2022-05-17 12:45:26 +0200

    fix: build

--- packages/google-types/src/diagram.ts
+++ packages/google-types/src/diagram.ts
@@ -2,4 +2,4 @@ import { BaseModels } from '@voiceflow/base-types';
 
 import { AnyStep } from './node';
 
-export interface Diagram extends BaseModels.Diagram.Model<BaseModels.BaseBlock | BaseModels.Actions | AnyStep> {}
+export interface Diagram extends BaseModels.Diagram.Model<BaseModels.BaseBlock | BaseModels.BaseActions | AnyStep> {}
