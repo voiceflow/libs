9dddf2124b84b3eeac7a82ab918386e9de95069d
Jonah Snider <jonah@jonahsnider.com>
2022-05-20 08:00:10 -0700

    feat(base-types): fix exit step log

--- packages/base-types/src/runtimeLogs/logs/steps/exit.ts
+++ packages/base-types/src/runtimeLogs/logs/steps/exit.ts
@@ -4,11 +4,21 @@ import { BaseStepLog } from '../base';
 import { StepLogKind } from '../kinds';
 import { LogLevel } from '../levels';
 
-export type ExitStepLog = BaseStepLog<
-  StepLogKind.EXIT,
-  {
-    /** The state of the program on exit. */
-    state: EmptyObject;
-  },
-  LogLevel.VERBOSE
->;
+export type ExitStepLog =
+  // Non-verbose mode doesn't include the state
+  | BaseStepLog<
+      StepLogKind.EXIT,
+      {
+        state: null;
+      },
+      LogLevel.INFO
+    >
+  // Verbose mode
+  | BaseStepLog<
+      StepLogKind.EXIT,
+      {
+        /** The state of the program on exit. */
+        state: EmptyObject;
+      },
+      LogLevel.VERBOSE
+    >;
