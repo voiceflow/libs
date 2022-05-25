0f0d6305d6a9a6cd1eddf401c0b709a64a5faaa3
Daniel Liu <danieliu3120@gmail.com>
2022-05-25 10:29:09 -0400

    Update packages/base-types/src/models/apiKey.ts
    Co-authored-by: Tyler Stewart <hello@tylerstewart.ca>

--- packages/base-types/src/models/apiKey.ts
+++ packages/base-types/src/models/apiKey.ts
@@ -27,5 +27,5 @@ export interface Model<Data extends AnyRecord = AnyRecord> {
   data?: Data;
   scopes: string[];
   permissions: string[];
-  secondaryKeyID: string;
+  secondaryKeyID?: string | null;
 }
