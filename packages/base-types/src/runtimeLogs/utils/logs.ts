b92cf594f78c585416f82803efdfcbe4122c30ed
Jonah Snider <jonah@jonahsnider.com>
2022-05-27 10:19:40 -0700

    feat(base-types): add `LogLevel.OFF` option

--- packages/base-types/src/runtimeLogs/utils/logs.ts
+++ packages/base-types/src/runtimeLogs/utils/logs.ts
@@ -1,13 +1,14 @@
 import { LogLevel } from '../logs/levels';
 
 const logLevelValues: Readonly<Record<LogLevel, number>> = {
+  [LogLevel.OFF]: -1,
   [LogLevel.ERROR]: 0,
   [LogLevel.WARN]: 1,
   [LogLevel.INFO]: 2,
   [LogLevel.VERBOSE]: 3,
 };
 
-/** Returns the number (non-negative integer) value of `level`. A higher number is more verbose. `0` is the least verbose and most "important". */
+/** Returns the number (non-negative integer) value of `level`. A higher number is more verbose. `0` is the least verbose and most "important". The value `-1` is used to disable logging. */
 export const getValueForLogLevel = (level: LogLevel): number => logLevelValues[level];
 
 const ALL_LOG_LEVELS: ReadonlySet<LogLevel> = new Set(Object.values(LogLevel));
