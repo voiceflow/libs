0cc86240cbf6d4ee50e1c418b537209d7a3f4fd4
Jonah Snider <jonah@jonahsnider.com>
2022-05-27 10:10:18 -0700

    feat(base-types): add `isLogLevel()` type guard

--- packages/base-types/src/runtimeLogs/utils/logs.ts
+++ packages/base-types/src/runtimeLogs/utils/logs.ts
@@ -9,3 +9,9 @@ const logLevelValues: Readonly<Record<LogLevel, number>> = {
 
 /** Returns the number (non-negative integer) value of `level`. A higher number is more verbose. `0` is the least verbose and most "important". */
 export const getValueForLogLevel = (level: LogLevel): number => logLevelValues[level];
+
+const ALL_LOG_LEVELS: ReadonlySet<LogLevel> = new Set(Object.values(LogLevel));
+
+export const isLogLevel = (level: string): level is LogLevel => {
+  return ALL_LOG_LEVELS.has(level as any);
+};
