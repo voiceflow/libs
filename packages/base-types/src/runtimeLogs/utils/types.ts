7a2d52195c7b7e44cdaaa8cfd0b6ee2585e4306c
Jonah Snider <jonah@jonahsnider.com>
2022-06-14 19:40:50 -0700

    feat(base-types): update runtime logging types for batch 1 of steps

--- packages/base-types/src/runtimeLogs/utils/types.ts
+++ packages/base-types/src/runtimeLogs/utils/types.ts
@@ -7,3 +7,17 @@ export interface PathReference {
   componentName: StepLogKind;
   stepID: string;
 }
+
+/** A common interface for representing a before & after change of a value. */
+export interface ValueChange<T> {
+  before: T;
+  after: T;
+}
+
+/**
+ * An abstract interface for something that includes changed variables.
+ * This enforces a consistent naming scheme of the property.
+ */
+export interface ChangedVariables<V, K extends PropertyKey = string> {
+  changedVariables: Record<K, ValueChange<V>>;
+}
