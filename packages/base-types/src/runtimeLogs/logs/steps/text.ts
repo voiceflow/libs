d63aac2c20546402c10257659e198e5a46496980
Jonah Snider <jonah@jonahsnider.com>
2022-06-17 05:35:13 +0000

    feat(base-types): update runtime logging types for batch 2 of steps (VF-3554) (#305)
    **Fixes or implements VF-3554**
    **Fixes or implements VF-3824**
    **Fixes or implements VF-3825**
    **Fixes or implements VF-3828**
    **Fixes or implements VF-3832**
    
    ### Brief description. What is this change?
    
    updates the types to facilitate implementing runtime logging for the condition, flow, and text steps
    
    ### Related PRs
    
    - https://github.com/voiceflow/general-runtime/pull/338
    
    ### Checklist
    
    - [ ] this is a breaking change and should publish a new major version
    - [ ] appropriate tests have been written

--- packages/base-types/src/runtimeLogs/logs/steps/text.ts
+++ packages/base-types/src/runtimeLogs/logs/steps/text.ts
@@ -1,9 +1,12 @@
+import { Text as TextNode } from '@base-types/node';
+
 import { BaseStepLog } from '../base';
 import { StepLogKind } from '../kinds';
 
 export type TextStepLog = BaseStepLog<
   StepLogKind.TEXT,
   {
-    text: string;
+    plainContent: string;
+    richContent: TextNode.TextData;
   }
 >;
