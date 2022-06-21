018145919fda0c33e2dd18fa68e016c292e4b4b4
Evgeny Zaytsev <overhawlin@gmail.com>
2022-06-21 12:09:44 +0000

    feat: move mappings from google service to libs (VF-000) (#309)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-000**
    
    ### Brief description. What is this change?
    
    move mappings and some utils from google service to libs since they will be used on the FE
    
    ### Checklist
    
    - [ ] this is a breaking change and should publish a new major version
    - [ ] appropriate tests have been written

--- packages/google-types/src/utils/index.ts
+++ packages/google-types/src/utils/index.ts
@@ -1,4 +1,5 @@
 export * as invocationName from './invocationName';
+export * as mappings from './mappings';
 export * as node from './node';
 export * as step from './step';
 export * as voice from './voice';
