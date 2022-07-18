49e68fa25109334d5f454a5deb6ed2ea8c7aa1b6
Filipe Merker <filipe.merker@voiceflow.com>
2022-07-18 09:51:24 -0300

    fix: change filename

--- packages/chat-types/src/node/index.ts
+++ packages/chat-types/src/node/index.ts
@@ -1,7 +1,7 @@
 export * as Buttons from './buttons';
 export * as Capture from './capture';
 export * as CaptureV2 from './captureV2';
-export * as CardV2 from './CardV2';
+export * as CardV2 from './cardV2';
 export * as Carousel from './carousel';
 export * as Interaction from './interaction';
 export * as Prompt from './prompt';
