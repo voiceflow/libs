0de56ba0f43e517c32bd10fad5479d7a88448924
Filipe Merker <filipe.merker@voiceflow.com>
2022-07-19 11:10:31 -0300

    fix: remove invalid types from chat-types

--- packages/chat-types/src/node/cardV2.ts
+++ packages/chat-types/src/node/cardV2.ts
@@ -1,6 +1,6 @@
 import { BaseNode, Nullable } from '@voiceflow/base-types';
 
-import { NodeNoMatch, NodeNoReply, StepNoMatch, StepNoReply } from './utils';
+import { StepNoMatch, StepNoReply } from './utils';
 
 export interface StepData extends BaseNode.CardV2.StepData {
   noReply?: Nullable<StepNoReply>;
@@ -8,8 +8,3 @@ export interface StepData extends BaseNode.CardV2.StepData {
 }
 
 export interface Step<Data = StepData> extends BaseNode.CardV2.Step<Data> {}
-
-export interface Node extends BaseNode.CardV2.Node {
-  noReply?: Nullable<NodeNoReply>;
-  noMatch?: Nullable<NodeNoMatch>;
-}
