5c1e37a07f869f603f480d188c2752c115e0786e
Vinicius Dacal <viniciusldacal@gmail.com>
2022-07-20 17:26:19 +0000

    feat: add carousel types to google-types (CT-601) (#332)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CT-601**
    
    ### Brief description. What is this change?
    
     - Added the types following the pattern we already have for existent card

---
+++ packages/google-types/src/node/carousel/index.ts
@@ -0,0 +1,9 @@
+import { ChatNode, ChatStep, ChatStepData } from './chat';
+
+export * from './chat';
+
+export type Step = ChatStep;
+
+export type StepData = ChatStepData;
+
+export type Node = ChatNode;
--- packages/google-types/src/node/index.ts
+++ packages/google-types/src/node/index.ts
@@ -3,6 +3,7 @@ import { BaseNode } from '@voiceflow/base-types';
 import * as Buttons from './buttons';
 import * as Capture from './capture';
 import * as CaptureV2 from './captureV2';
+import * as Carousel from './carousel';
 import * as Interaction from './interaction';
 import * as Prompt from './prompt';
 import * as Speak from './speak';
@@ -11,13 +12,23 @@ import * as Stream from './stream';
 export * as Buttons from './buttons';
 export * as Capture from './capture';
 export * as CaptureV2 from './captureV2';
+export * as Carousel from './carousel';
 export * as Interaction from './interaction';
 export * as Prompt from './prompt';
 export * as Speak from './speak';
 export * as Stream from './stream';
 
-export type AnyExtendedStep = Speak.Step | Prompt.Step | Capture.Step | CaptureV2.Step | Interaction.Step | Stream.Step | Buttons.Step;
-export type AnyExtendedNode = Speak.Node | Capture.Node | CaptureV2.Node | Interaction.Node | Stream.Node;
+export type AnyExtendedStep =
+  | Speak.Step
+  | Prompt.Step
+  | Capture.Step
+  | CaptureV2.Step
+  | Interaction.Step
+  | Stream.Step
+  | Buttons.Step
+  | Carousel.Step;
+
+export type AnyExtendedNode = Speak.Node | Capture.Node | CaptureV2.Node | Interaction.Node | Stream.Node | Carousel.Node;
 
 export type AnyStep = BaseNode.AnyCommonStep | AnyExtendedStep | BaseNode.Card.Step;
 export type AnyNode = BaseNode.AnyCommonNode | AnyExtendedNode | BaseNode.Card.Node;
