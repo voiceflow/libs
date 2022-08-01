6b8fc486d54255b39c46778e8c563a06d63bae67
Yauheni <overhawlin@gmail.com>
2022-08-01 16:51:22 +0200

    fix: typo

--- packages/api-sdk/src/resources/version/domain.ts
+++ packages/api-sdk/src/resources/version/domain.ts
@@ -37,24 +37,24 @@ class DomainResource extends CrudNestedResource<string, BaseModels.Version.Domai
     return this._patch<T>(versionID, id, body);
   }
 
-  public async delete(projectID: string, id: string): Promise<string> {
-    return this._delete(projectID, id);
+  public async delete(versionID: string, id: string): Promise<string> {
+    return this._delete(versionID, id);
   }
 
-  public async topicAdd(projectID: string, id: string, topicID: string): Promise<string> {
-    const { data } = await this.fetch.post<string>(`${this._getCRUDEndpoint(projectID, id)}/topics`, { topicID });
+  public async topicAdd(versionID: string, id: string, topicID: string): Promise<string> {
+    const { data } = await this.fetch.post<string>(`${this._getCRUDEndpoint(versionID, id)}/topics`, { topicID });
 
     return data;
   }
 
-  public async topicRemove(projectID: string, id: string, topicID: string): Promise<string> {
-    const { data } = await this.fetch.delete<string>(`${this._getCRUDEndpoint(projectID, id)}/topics/${topicID}`);
+  public async topicRemove(versionID: string, id: string, topicID: string): Promise<string> {
+    const { data } = await this.fetch.delete<string>(`${this._getCRUDEndpoint(versionID, id)}/topics/${topicID}`);
 
     return data;
   }
 
-  public async topicReorder(projectID: string, id: string, topicID: string, body: { toIndex: number }): Promise<string[]> {
-    const { data } = await this.fetch.patch<string[]>(`${this._getCRUDEndpoint(projectID, id)}/topics/${topicID}/reorder`, body);
+  public async topicReorder(versionID: string, id: string, topicID: string, body: { toIndex: number }): Promise<string[]> {
+    const { data } = await this.fetch.patch<string[]>(`${this._getCRUDEndpoint(versionID, id)}/topics/${topicID}/reorder`, body);
 
     return data;
   }
