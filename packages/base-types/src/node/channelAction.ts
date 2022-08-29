4175b88cd013f70a37b7e633dda909ed1f747399
Tyler Stewart <hello@tylerstewart.ca>
2022-08-29 16:58:11 +0000

    feat: channel action trace (PL-41) (#360)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements PL-41**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    
    
    Co-authored-by: Tyler Stewart <tyler.stewart@voiceflow.com>

--- packages/base-types/src/node/channelAction.ts
+++ packages/base-types/src/node/channelAction.ts
@@ -1,9 +1,9 @@
-import { Nullable } from '@voiceflow/common';
+import { AnyRecord, Nullable } from '@voiceflow/common';
 
 import { NodeType } from './constants';
 import { BaseEvent, BaseNode, BasePort, BasePortList, BaseStep, BaseStepPorts, NodeID } from './utils';
 
-interface StepData<Payload = unknown> {
+interface StepData<Payload = AnyRecord> {
   stop?: boolean;
   payload: Payload;
   defaultPath?: number;
@@ -15,7 +15,7 @@ export interface StepPort<Event = BaseEvent> extends BasePort {
 
 interface StepPorts<Event> extends BaseStepPorts<Record<string, StepPort<Event>>, StepPort<Event>[]> {}
 
-export interface Step<Payload = unknown, Event = BaseEvent> extends BaseStep<StepData<Payload>, StepPorts<Event>, BasePortList<StepPort<Event>>> {
+export interface Step<Payload = AnyRecord, Event = BaseEvent> extends BaseStep<StepData<Payload>, StepPorts<Event>, BasePortList<StepPort<Event>>> {
   type: NodeType.CHANNEL_ACTION;
 }
 
@@ -29,7 +29,7 @@ export interface Node<Event = BaseEvent> extends BaseNode {
   type: NodeType.CHANNEL_ACTION;
   data: {
     name: string;
-    payload: unknown;
+    payload: AnyRecord;
   };
   stop: boolean;
   defaultPath?: number; // index starting from 0
