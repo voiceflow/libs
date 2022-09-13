9b2b3f2febaae3f96bc011daaf80fb881c161118
Brennan Lu <brennan.lu314@gmail.com>
2022-09-13 13:51:20 -0400

    feat: added separate custom action and pointer steps

--- packages/base-types/src/node/customAction.ts
+++ packages/base-types/src/node/customAction.ts
@@ -1,20 +1,59 @@
+import { Nullable } from '@voiceflow/common';
 import { NodeType } from './constants';
-import { BaseNoMatchStepData, BaseNoReplyStepData, BaseStep, BaseStepPorts, BuiltInNextPort, BuiltInNoMatchNoReplyPorts } from './utils';
+import { BaseStep, BaseStepPorts, BasePort, BaseEvent, BaseNode, NodeID } from './utils';
 
-export interface StepBuiltInPorts extends BuiltInNextPort, BuiltInNoMatchNoReplyPorts {}
+/**
+ * Custom Action v1.5, built for the Custom Blocks MVP.
+ */
 
-export interface StepPorts extends BaseStepPorts<StepBuiltInPorts> {}
-
-export interface StepData extends BaseNoMatchStepData, BaseNoReplyStepData {
-  sourceID: string;
-  parameters: Record<string, string>;
-  name: string;
-  payload: string;
-  stop?: boolean;
-  paths: { label: string }[];
-  defaultPath?: number;
+export interface Path {
+    /**
+     * Human-readable identifier for the path.
+     */
+    label: string;
 }
 
-export interface Step<Data = StepData> extends BaseStep<Data, StepPorts> {
-  type: NodeType.CUSTOM_ACTION;
+export interface StepData<Payload = unknown> {
+    _v: 1.5;
+    stop?: boolean;
+    paths: Path[];
+    payload: Payload;
+    defaultPath?: number;
+
+    /**
+     * User-defined parameters that are substituted into the JSON payload of
+     * the custom action.
+     */
+    parameters: string[];
+}
+
+export interface StepPort<Event = BaseEvent> extends BasePort {
+    data: { event?: Event };
+}
+
+export interface StepPorts<Event> extends BaseStepPorts<
+    Record<string,
+        StepPort<Event>>,
+    StepPort<Event>[]
+> { }
+
+export interface Step<Payload = unknown, Event = BaseEvent> extends BaseStep<
+    StepData<Payload>,
+    StepPorts<Event>
+> {
+    type: NodeType.CUSTOM_ACTION;
+}
+
+export interface NodePath<Event = BaseEvent> {
+    label?: string;
+    event?: Event;
+    nextID: Nullable<NodeID>;
+}
+
+export interface Node<Event = BaseEvent> extends BaseNode {
+    _v: 1.5;
+    stop: boolean;
+    paths: Array<NodePath<Event>>;
+    payload: unknown;
+    defaultPath?: number; // index starting from 0
 }
---
+++ packages/base-types/src/node/pointer.ts
@@ -0,0 +1,15 @@
+import { NodeType } from './constants';
+import { BaseNoMatchStepData, BaseNoReplyStepData, BaseStep, BaseStepPorts, BuiltInNextPort, BuiltInNoMatchNoReplyPorts } from './utils';
+
+export interface StepBuiltInPorts extends BuiltInNextPort, BuiltInNoMatchNoReplyPorts {}
+
+export interface StepPorts extends BaseStepPorts<StepBuiltInPorts> {}
+
+export interface StepData extends BaseNoMatchStepData, BaseNoReplyStepData {
+  sourceID: string;
+  parameters: Record<string, string>;
+}
+
+export interface Step<Data = StepData> extends BaseStep<Data, StepPorts> {
+  type: NodeType.POINTER;
+}
