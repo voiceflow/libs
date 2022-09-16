9c4013e38d52127a371e24e62581dc72dd6fa3bb
Tyler Stewart <tyler.stewart@voiceflow.com>
2022-09-16 14:59:32 -0600

    chore: lint

--- packages/base-types/src/models/index.ts
+++ packages/base-types/src/models/index.ts
@@ -1,9 +1,9 @@
 export * as ApiKey from './apiKey';
 export * from './base';
+export * as CustomBlock from './customBlock';
 export * as Diagram from './diagram';
 export * as Program from './program';
 export * as Project from './project';
 export * as Transcript from './transcripts';
 export * as VariableState from './variableState';
 export * as Version from './version';
-export * as CustomBlock from './customBlock';
