8dcb0f1124c521d0a6e06b1af99223ecce9a0beb
Tyler Han <tylerhan97@gmail.com>
2022-10-17 13:00:20 +0200

    fix: add chat publishing settings (CT-000)

--- packages/voiceflow-types/src/version/chat/settings.ts
+++ packages/voiceflow-types/src/version/chat/settings.ts
@@ -1,3 +1,4 @@
+import { BaseVersion } from '@voiceflow/base-types';
 import { ChatVersion } from '@voiceflow/chat-types';
 
 import { BaseSettings, defaultBaseSettings } from '../base';
@@ -5,6 +6,18 @@ import { BaseSettings, defaultBaseSettings } from '../base';
 export interface SharedChatSettings extends ChatVersion.Settings {}
 
 export interface ChatSettings extends SharedChatSettings, BaseSettings {}
+
+export interface ChatPublishing extends BaseVersion.Publishing {
+  title?: string;
+  image?: string;
+  color?: string;
+  avatar?: string;
+  spacing?: { side: number; bottom: number };
+  launcher?: string;
+  position?: string;
+  persistence?: boolean;
+  description?: string;
+}
 
 export const defaultSharedChatSettings = (settings: Partial<SharedChatSettings> = {}): SharedChatSettings => ({
   ...ChatVersion.defaultSettings(settings),
