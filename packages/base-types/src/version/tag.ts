e65cd2b60e413972fd358cbac4026411df5e5c4e
Brennan Lu <brennan.lu314@gmail.com>
2022-11-11 15:43:04 -0500

    feat: forcing version deploy

--- packages/base-types/src/version/tag.ts
+++ packages/base-types/src/version/tag.ts
@@ -1,11 +1,11 @@
 export enum VersionTag {
   /**
-   * Tag value for Voiceflow version deployed in the production slot
+   * Tag value for Voiceflow version deployed in the production slot.
    */
   PRODUCTION = 'production',
 
   /**
-   * Tag value for Voiceflow version deployed in the development slot
+   * Tag value for Voiceflow version deployed in the development slot.
    */
   DEVELOPMENT = 'development',
 }
