2fcc0197bf68419cb24c00c616d57d8b2d2baa7d
Ben Teichman <ben@voiceflow.com>
2022-11-28 22:01:07 +0100

    test: fix test

--- packages/fetch/src/main.ts
+++ packages/fetch/src/main.ts
@@ -1,5 +1,5 @@
+export * from './client-configuration.interface';
 export * from './fetch.client';
 export * from './fetch.interface';
-export * from './client-configuration.interface';
 export * from './http-method.enum';
 export * from './request-options.interface';
