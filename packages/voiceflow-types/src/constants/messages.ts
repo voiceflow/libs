a3ad000e53f753c84f0e8f0805df9847bbdfd732
Vinicius Dacal <viniciusldacal@gmail.com>
2023-01-18 15:24:37 +0000

    fix: add period to end of sentence (CT-000) (#414)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CT-000**
    
    ### Brief description. What is this change?
    
    Add period to end of the sentence

--- packages/voiceflow-types/src/constants/messages.ts
+++ packages/voiceflow-types/src/constants/messages.ts
@@ -1,4 +1,4 @@
 export const defaultMessages = {
   globalNoReply: 'Are you still there?',
-  globalNoMatch: 'Sorry, I didn’t get that. Please try again',
+  globalNoMatch: 'Sorry, I didn’t get that. Please try again.',
 };
