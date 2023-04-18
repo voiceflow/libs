69a31db521a97cd404ae9ce8509c9830ff9097cf
Tyler Han <tylerhan97@gmail.com>
2023-04-18 12:12:52 -0400

    fix: add codepath property to code step

--- packages/base-types/src/node/code.ts
+++ packages/base-types/src/node/code.ts
@@ -1,8 +1,15 @@
 import { NodeType } from './constants';
 import { BaseNode, BaseStep, NodeSuccessFailID, SuccessFailStepPorts } from './utils';
 
+export interface CodePath {
+  key: string;
+  label: string;
+}
+
+// actions have no paths
 export interface StepData {
   code: string;
+  paths?: CodePath[];
 }
 
 export interface StepPorts extends SuccessFailStepPorts<[]> {}
