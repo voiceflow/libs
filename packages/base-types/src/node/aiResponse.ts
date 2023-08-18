cfdf5ad1eb1ba0cac94f897be0d2c06cead84eca
Tyler Han <tylerhan97@gmail.com>
2023-08-17 23:43:08 -0400

    fix: add else port to ai response (LLM-000)

--- packages/base-types/src/node/aiResponse.ts
+++ packages/base-types/src/node/aiResponse.ts
@@ -1,13 +1,13 @@
 import { AIContextParams, AIKnowledgeParams, AIModelParams } from '../utils/ai';
 import { NodeType } from './constants';
-import { BaseNode, BaseStep, NodeNextID } from './utils';
+import { BaseNode, BaseStep, BuiltInNextPort, BuiltInNoMatchPort, NodeElseID, NodeNextID } from './utils';
 
 export interface StepData extends AIModelParams, AIContextParams, AIKnowledgeParams {}
 
-export interface Step<Data = StepData> extends BaseStep<Data> {
+export interface Step<Data = StepData> extends BaseStep<Data, BuiltInNextPort & BuiltInNoMatchPort> {
   type: NodeType.AI_RESPONSE;
 }
 
-export interface Node extends BaseNode, StepData, NodeNextID {
+export interface Node extends BaseNode, StepData, NodeNextID, NodeElseID {
   type: NodeType.AI_RESPONSE;
 }
