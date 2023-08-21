1c95eaa3605961e79b084fdea102466eafb0592a
Yauheni <overhawlin@gmail.com>
2023-08-21 13:02:36 +0200

    fix: potential fix for the (CV3-267)

--- packages/common/src/crypto/murmurhash.ts
+++ packages/common/src/crypto/murmurhash.ts
@@ -4,7 +4,7 @@ import { MurmurHash3 } from 'murmurhash-wasm';
 export class MurmurHash {
   public static hash(key: string, seed = 0): string {
     const hash = MurmurHash3.hash32(key, seed);
-    const value = hash.readUInt32BE();
+    const value = hash.readUInt32BE(0);
     const hex = value.toString(16);
 
     return hex2abc(hex);
