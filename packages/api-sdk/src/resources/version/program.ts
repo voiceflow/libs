dfcc2c8b527deecb22e963c6b248282542193d26
Vinicius Dacal <viniciusldacal@gmail.com>
2023-09-25 14:21:22 +0000

    feat: add new programs and diagrams endpoint under version (CV3-389) (#454)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-389**
    
    ### Brief description. What is this change?
     - These new endpoints will be used by general-service to read diagrams and programs
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

---
+++ packages/api-sdk/src/resources/version/program.ts
@@ -0,0 +1,25 @@
+import Fetch from '@api-sdk/fetch';
+import { BaseModels } from '@voiceflow/base-types';
+
+import CrudNestedResource from '../crudNested';
+
+const ENDPOINT = 'programs';
+
+export type ModelKey = 'diagramID';
+
+class ProgramResource extends CrudNestedResource<string, BaseModels.Program.Model, ModelKey, ProgramResource> {
+  constructor(fetch: Fetch, { parentEndpoint }: { parentEndpoint: string }) {
+    super({
+      fetch,
+      clazz: ProgramResource,
+      endpoint: ENDPOINT,
+      clazzOptions: { parentEndpoint },
+    });
+  }
+
+  public async get(versionID: string, diagramID: string): Promise<BaseModels.Program.Model> {
+    return this._getByID<BaseModels.Program.Model>(versionID, diagramID);
+  }
+}
+
+export default ProgramResource;
