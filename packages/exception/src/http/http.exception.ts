0ba0d958e62deac303a8ef99179acfe899be28d3
Zhi Heng (Brennan) Lu <brennan.lu314@gmail.com>
2023-10-12 18:17:02 +0000

    feat: adding serialized check (CV3-289) (#461)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-289**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Adding a new static method to `HTTPException` to check if a plain object is an instance of a serialized `HTTPException`.
    
    The reason this method should be added as part of the `HTTPException` API is because the `HTTPException` is often serialized and transmitted over the wire to a different service. The client service which receives the serialized `HTTPException` needs a method to detect that the error payload is of shape `SerializedHTTPException` and not something else like a basic JavaScript `Error`. This is a very common use-case. 
    
    Without this method, every client has to implement the duck-typing check for `SerializedHTTPException` to proper handle this exception. This creates duplication.

--- packages/exception/src/http/http.exception.ts
+++ packages/exception/src/http/http.exception.ts
@@ -2,9 +2,28 @@ import { ErrorMessage } from '@exception/error-message.interface';
 import { ErrorPayload } from '@exception/error-payload.interface';
 import { InternalException } from '@exception/internal.exception';
 
-export class HTTPException extends InternalException {
+export interface SerializedHTTPException {
+  message: string;
+  statusCode: number;
+  statusText: string;
+}
+
+export class HTTPException extends InternalException implements SerializedHTTPException {
   public static instanceOf(err: any): err is HTTPException {
     return err instanceof HTTPException;
+  }
+
+  private static hasRequiredProperties(err: unknown): err is Record<keyof SerializedHTTPException, unknown> {
+    return typeof err === 'object' && err !== null && 'statusText' in err && 'message' in err && 'statusCode' in err;
+  }
+
+  public static isSerializedInstance(err: unknown): err is SerializedHTTPException {
+    return (
+      HTTPException.hasRequiredProperties(err) &&
+      typeof err.statusText === 'string' &&
+      typeof err.message === 'string' &&
+      typeof err.statusCode === 'number'
+    );
   }
 
   public statusCode: number;
