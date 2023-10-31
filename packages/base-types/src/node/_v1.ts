0c77d3f5af71ceb72a5f66d721a9f1c529b9d3eb
Tyler Han <tylerhan97@gmail.com>
2023-10-31 10:54:55 -0400

    fix: add meta field to v1

--- packages/base-types/src/node/_v1.ts
+++ packages/base-types/src/node/_v1.ts
@@ -32,6 +32,7 @@ export interface Node<Event = BaseEvent> extends BaseNode {
   _v: 1;
   stop: boolean;
   paths: Array<NodePath<Event>>;
+  meta: unknown;
   payload: unknown;
   defaultPath?: number; // index starting from 0
 }
