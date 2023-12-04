79be7e6d1047d096e02a96536d5197d6294e98c3
Tyler Han <tylerhan97@gmail.com>
2023-12-04 18:09:33 -0500

    fix: add gpt 4 turbo (ACT-003)

--- packages/base-types/src/utils/ai.ts
+++ packages/base-types/src/utils/ai.ts
@@ -2,12 +2,13 @@ export enum GPT_MODEL {
   DaVinci_003 = 'text-davinci-003',
   GPT_3_5_turbo = 'gpt-3.5-turbo',
   GPT_4 = 'gpt-4',
+  GPT_4_turbo = 'gpt-4-turbo',
   CLAUDE_V1 = 'claude-v1',
   CLAUDE_V2 = 'claude-v2',
   CLAUDE_INSTANT_V1 = 'claude-instant-v1',
 }
 
-export const ChatModels = [GPT_MODEL.GPT_3_5_turbo, GPT_MODEL.GPT_4];
+export const ChatModels = [GPT_MODEL.GPT_3_5_turbo, GPT_MODEL.GPT_4, GPT_MODEL.GPT_4_turbo];
 
 export enum PROMPT_MODE {
   PROMPT = 'prompt',
