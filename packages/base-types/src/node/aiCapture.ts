03f3128d2abb8d5456c1a97799079ea9723e0b93
Tyler Han <tylerhan97@gmail.com>
2023-12-19 05:35:52 -0500

    fix: ai capture ports

--- packages/base-types/src/node/aiCapture.ts
+++ packages/base-types/src/node/aiCapture.ts
@@ -1,43 +1,27 @@
+import { BaseStepPorts } from '@base-types/models';
+
 import { AIModelParams } from '../utils/ai';
 import { NodeType } from './constants';
 import {
   BaseNode,
   BaseNoMatchNodeData,
-  BaseNoMatchStepData,
   BaseNoReplyNodeData,
-  BaseNoReplyStepData,
   BaseStep,
-  BaseStepPorts,
-  BuiltInFailPort,
   BuiltInNextPort,
   BuiltInNoMatchNoReplyPorts,
-  NodeElseID,
   NodeIntentScope,
   NodeNextID,
-  StepIntentScope,
 } from './utils';
 
-export interface StepData extends AIModelParams {}
-
-export interface Step extends BaseStep<StepData, BaseStepPorts<BuiltInNextPort & BuiltInFailPort>> {
-  type: NodeType.AI_CAPTURE;
-
+export interface StepData extends AIModelParams {
   rules: string[];
   entities: string[];
   exitScenerios: string[];
 }
 
-export interface Node extends BaseNode, StepData, NodeNextID, NodeElseID {
+export interface Step extends BaseStep<StepData, BaseStepPorts<BuiltInNextPort & BuiltInNoMatchNoReplyPorts>> {
   type: NodeType.AI_CAPTURE;
 }
-
-export interface BaseCaptureData extends BaseNoReplyStepData, StepIntentScope, BaseNoMatchStepData {}
-
-export interface StepData extends AIModelParams {}
-
-export interface StepBuiltInPorts extends BuiltInNextPort, BuiltInNoMatchNoReplyPorts {}
-
-export interface StepPorts extends BaseStepPorts<StepBuiltInPorts, []> {}
 
 export interface Node extends BaseNode, StepData, NodeNextID, NodeIntentScope, BaseNoReplyNodeData, BaseNoMatchNodeData {
   type: NodeType.AI_CAPTURE;
