3a793942c27b3b88a76f1e9a4921b63198a066c0
Tyler Han <tylerhan97@gmail.com>
2023-12-27 13:51:13 -0500

    fix: google-apis is only a dev dependency (CT-000) (#487)

--- packages/google-types/src/models.ts
+++ packages/google-types/src/models.ts
@@ -1,3 +1,3 @@
-import { oauth2_v2 } from 'googleapis/build/src/apis/oauth2/v2';
+import type { oauth2_v2 } from 'googleapis/build/src/apis/oauth2/v2';
 
 export interface GoogleProfile extends oauth2_v2.Schema$Userinfo {}
