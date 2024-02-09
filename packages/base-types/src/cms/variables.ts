82db3e53988b2e50b450ec99ca395bed589785c8
Zhi Heng (Brennan) Lu <brennan.lu314@gmail.com>
2024-02-09 21:17:01 +0000

    feat: added types for compiled cms variables (CV3-942) (#501)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements CV3-942**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Adds serialized `.cmsVariables` mapping to the surveyor context type. This is necessary to provide strict typing to

---
+++ packages/base-types/src/cms/variables.ts
@@ -0,0 +1,27 @@
+export declare const VariableDatatype: {
+  readonly ANY: 'any';
+  readonly TEXT: 'text';
+  readonly DATE: 'date';
+  readonly IMAGE: 'image';
+  readonly NUMBER: 'number';
+  readonly BOOLEAN: 'boolean';
+};
+
+export type VariableDatatype = typeof VariableDatatype[keyof typeof VariableDatatype];
+
+export interface BaseCompiledCMSVariable {
+  isSystem: boolean;
+  defaultValue: string;
+}
+
+export interface CompiledSystemCMSVariable extends BaseCompiledCMSVariable {
+  isSystem: true;
+}
+
+export interface CompiledUserCMSVariable extends BaseCompiledCMSVariable {
+  isSystem: false;
+  datatype: VariableDatatype;
+  isArray: boolean;
+}
+
+export type CompiledCMSVariable = CompiledSystemCMSVariable | CompiledUserCMSVariable;
