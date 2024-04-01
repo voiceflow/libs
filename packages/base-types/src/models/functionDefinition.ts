04e60cbdf54b7f05695b40113ac7d6283e3a9c3b
Brennan Lu <brennan.lu314@gmail.com>
2024-04-01 16:00:25 -0400

    feat: refactored used functions map into function definitions

---
+++ packages/base-types/src/models/functionDefinition.ts
@@ -0,0 +1,12 @@
+import { VariableDatatype } from "@base-types/cms/variables";
+
+export interface FunctionCompiledVariableDeclaration {
+    type: VariableDatatype;
+}
+
+export interface FunctionCompiledDefinition {
+    codeId: string;
+    inputVars: Record<string, FunctionCompiledVariableDeclaration>;
+    outputVars: Record<string, FunctionCompiledVariableDeclaration>;
+    pathCodes: string[];
+}
