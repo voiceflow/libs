584149e9f0ce23fdd214f160999aa1b160453758
Zhi Heng (Brennan) Lu <brennan.lu314@gmail.com>
2024-05-01 17:57:14 +0000

    feat: fixing inaccurate request types (PL-753) (#522)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements PL-753**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->
    
    Updated all request types to have types that better reflect the actual data sent to and the design assumptions of `general-runtime`. 
    
    Deprecated type-guards from `@voiceflow/base-types` due to their unreliability.

--- packages/base-types/src/request/index.ts
+++ packages/base-types/src/request/index.ts
@@ -30,15 +30,20 @@ export interface Entity {
   name: string; // name of matched entity
   value: string; // inferred value of matched entity
   query?: string; // raw value of matched entity
+
+  /**
+   * @deprecated This value is no longer generated. Please remove all code relying on
+   * LUIS NLU to produce `verboseValue`
+   */
   verboseValue?: VerboseValue[]; // more detailed results from LUIS
 }
 
 export interface LabelRequestPayload {
   label?: string;
 }
-export interface BaseRequest<Payload = unknown> {
+export interface BaseRequest {
   type: string;
-  payload: Payload;
+  payload?: unknown;
   diagramID?: string; // particular topic to match against
 }
 
@@ -46,38 +51,43 @@ export interface LaunchRequestPayload {
   persona?: string;
 }
 
-export interface LaunchRequest extends BaseRequest<LaunchRequestPayload | undefined> {
+export interface LaunchRequest extends BaseRequest {
   type: RequestType.LAUNCH;
+  payload?: LaunchRequestPayload;
 }
 
-export interface NoReplyRequest extends BaseRequest<undefined> {
+export interface NoReplyRequest extends Omit<BaseRequest, 'payload'> {
   type: RequestType.NO_REPLY;
 }
 
-export interface TextRequest extends BaseRequest<string> {
+export interface TextRequest extends BaseRequest {
   type: RequestType.TEXT;
+  payload: string;
 }
 
 interface ActionAndLabelRequestPayload extends ActionPayload, LabelRequestPayload {}
 
 export interface IntentRequestPayload extends ActionAndLabelRequestPayload {
-  query: string; // original text input
   intent: { name: string }; // matched intent name
   entities: Entity[]; // entities matches - multiple of the same entity name may be matched
+  query: string; // original text input
   confidence?: number; // 0-1 confidence of match;
   data?: Record<string, unknown>;
 }
 
-export interface IntentRequest extends BaseRequest<IntentRequestPayload> {
+export interface IntentRequest extends BaseRequest {
   type: RequestType.INTENT;
+  payload: IntentRequestPayload;
 }
 
-export interface GeneralRequest extends BaseRequest<ActionAndLabelRequestPayload> {
-  type: string; // the general request type is dynamic, used to m
+export interface GeneralRequest extends BaseRequest {
+  type: string; // the general request type is dynamic
+  payload?: ActionAndLabelRequestPayload;
 }
 
-export interface ActionRequest extends BaseRequest<ActionAndLabelRequestPayload> {
+export interface ActionRequest extends BaseRequest {
   type: RequestType.ACTION;
+  payload?: ActionAndLabelRequestPayload;
 }
 
 export interface BaseRequestButton<T extends BaseRequest = BaseRequest> {
@@ -104,18 +114,48 @@ export interface NodeButton {
   buttons?: AnyRequestButton[];
 }
 
+/**
+ * @deprecated This type guard is no longer an effective check if `request` is the specified type. The actual
+ * data being sent does not match the original type definition. This type guard will not be updated with a fix.
+ * Please use the type guards and equivalent DTOs in `@voiceflow/dtos` instead.
+ */
 export const isTextRequest = (request: BaseRequest): request is TextRequest => request.type === RequestType.TEXT;
 
+/**
+ * @deprecated This type guard is no longer an effective check if `request` is the specified type. The actual
+ * data being sent does not match the original type definition. This type guard will not be updated with a fix.
+ * Please use the type guards and equivalent DTOs in `@voiceflow/dtos` instead.
+ */
 export const isActionRequest = (request: BaseRequest): request is ActionRequest => request.type === RequestType.ACTION;
 
+/**
+ * @deprecated This type guard is no longer an effective check if `request` is the specified type. The actual
+ * data being sent does not match the original type definition. This type guard will not be updated with a fix.
+ * Please use the type guards and equivalent DTOs in `@voiceflow/dtos` instead.
+ */
 export const isLaunchRequest = (request: BaseRequest): request is LaunchRequest => request.type === RequestType.LAUNCH;
 
+/**
+ * @deprecated This type guard is no longer an effective check if `request` is the specified type. The actual
+ * data being sent does not match the original type definition. This type guard will not be updated with a fix.
+ * Please use the type guards and equivalent DTOs in `@voiceflow/dtos` instead.
+ */
 export const isNoReplyRequest = (request: BaseRequest): request is NoReplyRequest =>
   request.type === RequestType.NO_REPLY;
 
+/**
+ * @deprecated This type guard is no longer an effective check if `request` is the specified type. The actual
+ * data being sent does not match the original type definition. This type guard will not be updated with a fix.
+ * Please use the type guards and equivalent DTOs in `@voiceflow/dtos` instead.
+ */
 export const isIntentRequest = (request: BaseRequest): request is IntentRequest => request.type === RequestType.INTENT;
 
 const ALL_REQUEST_TYPES = Object.values(RequestType) as string[];
 
+/**
+ * @deprecated This type guard is no longer an effective check if `request` is the specified type. The actual
+ * data being sent does not match the original type definition. This type guard will not be updated with a fix.
+ * Please use the type guards and equivalent DTOs in `@voiceflow/dtos` instead.
+ */
 export const isGeneralRequest = (request: BaseRequest): request is GeneralRequest =>
   !ALL_REQUEST_TYPES.includes(request.type);
