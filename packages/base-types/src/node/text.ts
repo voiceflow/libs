9683c57ed241a34cf2807eba0102883d15cf2352
Yauheni <overhawlin@gmail.com>
2024-09-23 13:10:54 +0200

    feat: add audioSrc to text trace (COR-000)

--- packages/base-types/src/node/text.ts
+++ packages/base-types/src/node/text.ts
@@ -35,6 +35,7 @@ export interface Node extends BaseNode, NodeNextID {
 export interface TextTracePayload extends BaseResponseTrace {
   slate: TextData;
   delay?: number;
+  audioSrc?: string;
 }
 
 export interface TraceFrame extends BaseTraceFrame<TextTracePayload> {
