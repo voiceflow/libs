e607f69ed5ea88943d2b3d66f36cbdb5e02cfbd0
pmvrmc <pedro.matosdecarvalho@voiceflow.com>
2025-07-24 14:59:45 +0000

    fix: cleanup (COR-7009) (#586)
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    **Fixes or implements VF-XXX**
    
    ### Brief description. What is this change?
    
    <!-- Build up some context for your teammates on the changes made here and potential tradeoffs made and/or highlight any topics for discussion -->

--- packages/base-types/src/version/index.ts
+++ packages/base-types/src/version/index.ts
@@ -7,7 +7,6 @@ import { defaultSettings } from './settings';
 
 export * from './publishing';
 export * from './settings';
-export * from './tag';
 
 export interface PlatformData<Prompt = unknown> extends VersionModels.PlatformData<Settings<Prompt>, Publishing> {}
 
